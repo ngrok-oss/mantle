761d5763c989e246c67cf45ace19fe731b891437
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2023-07-29 02:40:54 -0500

    updates button (#1)
    * updates button
    
    * updates storybook workflow
    
    * adds codeowners

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { type ButtonHTMLAttributes, forwardRef } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
@@ -35,12 +35,13 @@ const buttonVariants = cva(
 
 export type ButtonProps = {
 	asChild?: boolean;
-} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+} & ButtonHTMLAttributes<HTMLButtonElement> &
 	VariantProps<typeof buttonVariants>;
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 	({ className, variant, size, asChild = false, ...props }, ref) => {
 		const Comp = asChild ? Slot : "button";
+
 		return (
 			<Comp
 				className={cn(buttonVariants({ variant, size, className }))}
