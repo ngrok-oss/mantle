55ce4494412fe7e11c0f9f87a839e430dfdff29f
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2023-07-29 03:24:49 -0500

    updates storybook deploy (#2)
    * updates storybook deploy
    
    * updates button type docs
    
    * more storybook and workflow updates
    
    * lock it down: only deploy storybook when merging to main

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -34,6 +34,20 @@ const buttonVariants = cva(
 );
 
 export type ButtonProps = {
+  /**
+   * Use the `asChild` prop to compose Radix's functionality onto alternative
+   * element types or your own React components.
+   * 
+   * All Radix primitive parts that render a DOM element accept an `asChild`
+   * prop. When `asChild` is set to `true`, Radix will not render a default DOM
+   * element, instead cloning the part's child and passing it the props and
+   * behavior required to make it functional.
+   * 
+   * asChild can be used as deeply as you need to. This means it is a great way
+   * to compose multiple primitive's behavior together.
+   * 
+   * @see https://www.radix-ui.com/docs/primitives/guides/composition#composition
+   */
 	asChild?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement> &
 	VariantProps<typeof buttonVariants>;
