52787a3639ff5527d16390022a5f3baf1a184021
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2023-08-22 23:04:29 -0500

    fixes vitest (#9)

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
 		},
 	},
 	test: {
+		globals: true,
 		environment: "jsdom",
 		setupFiles: "./src/test/setup.ts",
 		// you might want to disable it, if you don't have tests that rely on CSS
