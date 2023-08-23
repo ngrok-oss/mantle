52cd5ae55020bec0b86a461777be408ed5281682
Aaron Shekey <aaron@aaronshekey.com>
2023-08-23 16:20:10 -0500

    Button focus states

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,7 +7,7 @@ import type { WithAsChild } from "../types/as-child";
 import type { VariantProps } from "../types/variant-props";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center rounded-md font-medium border border-transparent ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+	"inline-flex items-center justify-center rounded-md font-medium border border-transparent ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-brand-primary-500/25 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50",
 	{
 		variants: {
 			priority: {
@@ -35,17 +35,19 @@ const buttonVariants = cva(
 			{
 				priority: "default",
 				state: "danger",
-				class: "border-danger-600 text-danger-600 hover:bg-danger-50 active:bg-danger-100",
+				class:
+					"border-danger-600 text-danger-600 hover:bg-danger-50 active:bg-danger-100 focus-visible:ring-danger-500/25",
 			},
 			{
 				priority: "primary",
 				state: "danger",
-				class: "bg-danger-500 hover:bg-danger-600 active:bg-danger-700",
+				class: "bg-danger-500 hover:bg-danger-600 active:bg-danger-700 focus-visible:ring-danger-500/25",
 			},
 			{
 				priority: "secondary",
 				state: "danger",
-				class: "bg-danger-50 border-danger-300 text-danger-900 hover:bg-danger-100 active:bg-danger-200",
+				class:
+					"bg-danger-50 border-danger-300 text-danger-900 hover:bg-danger-100 active:bg-danger-200 focus-visible:ring-danger-500/25",
 			},
 		],
 	},
