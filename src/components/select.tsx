4e6868502867ce803b7a4e25288795551581b99c
Aaron Shekey <aaron@aaronshekey.com>
2023-09-29 11:36:51 -0500

    Grab some select styling from another branch

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -33,7 +33,7 @@ const SelectTrigger = forwardRef<ElementRef<typeof Trigger>, ComponentPropsWitho
 		<Trigger
 			ref={ref}
 			className={cx(
-				"flex h-9 w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+				"flex h-10 w-full items-center justify-between rounded-md border border-gray-500 bg-white px-3 py-2 placeholder:text-gray-300 focus:border-brand-primary-500 focus:outline-none focus:ring-4 focus:ring-brand-primary-600/25 disabled:cursor-not-allowed disabled:opacity-50",
 				className,
 			)}
 			{...props}
@@ -53,7 +53,7 @@ const SelectContent = forwardRef<ElementRef<typeof Content>, ComponentPropsWitho
 			<Content
 				ref={ref}
 				className={cx(
-					"relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+					"relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white text-gray-900 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
 					position === "popper" &&
 						"data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
 					className,
@@ -88,7 +88,7 @@ const SelectOption = forwardRef<ElementRef<typeof Item>, ComponentPropsWithoutRe
 		<Item
 			ref={ref}
 			className={cx(
-				"relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+				"relative flex w-full cursor-pointer items-center rounded-sm py-1.5 pl-2 pr-2 text-sm outline-none focus:bg-brand-primary-500 focus:text-button data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
 				className,
 			)}
 			{...props}
