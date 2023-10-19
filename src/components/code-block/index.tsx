f00249bfa196821c0b1ec748f69226c1be113574
Aaron Shekey <aaron@aaronshekey.com>
2023-10-19 13:14:12 -0500

    More contrast

--- src/components/code-block/index.tsx
+++ src/components/code-block/index.tsx
@@ -159,7 +159,7 @@ const CodeBlockCopyButton = forwardRef<HTMLButtonElement, CodeBlockCopyButtonPro
 		return (
 			<button
 				type="button"
-				className={cx("absolute right-1.5 top-1.5 z-50 rounded-sm p-2 hover:bg-gray-100", className)}
+				className={cx("absolute right-1.5 top-1.5 z-50 rounded-sm p-2 hover:bg-gray-200", className)}
 				ref={ref}
 				style={style}
 				onClick={() => {
