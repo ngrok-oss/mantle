c5e4f63eceee1c66a8af53be7c162ab684748fb5
Cody A Price <cody@ngrok.com>
2023-10-31 01:01:39 -0500

    update inline snapshoots

--- packages/code-block/code.test.ts
+++ packages/code-block/code.test.ts
@@ -51,13 +51,13 @@ ngrok.listen(server).then(() => {
 			  "const ngrok = require(\\"@ngrok/ngrok\\");",
 			  "",
 			  "const server = http.createServer((req, res) => {",
-			  "  res.writeHead(200);",
-			  "  res.end(\\"Hello!\\");",
+			  "	res.writeHead(200);",
+			  "	res.end(\\"Hello!\\");",
 			  "});",
 			  "",
 			  "// Consumes authtoken from env automatically",
 			  "ngrok.listen(server).then(() => {",
-			  "  console.log(\\"url:\\", server.tunnel.url());",
+			  "	console.log(\\"url:\\", server.tunnel.url());",
 			  "});",
 			]
 		`);
@@ -86,13 +86,13 @@ ngrok.listen(server).then(() => {
 			  "const ngrok = require(\\"@ngrok/ngrok\\");",
 			  "",
 			  "const server = http.createServer((req, res) => {",
-			  "  res.writeHead(200);",
-			  "  res.end(\\"Hello!\\");",
+			  "	res.writeHead(200);",
+			  "	res.end(\\"Hello!\\");",
 			  "});",
 			  "",
 			  "// Consumes authtoken from env automatically",
 			  "ngrok.listen(server).then(() => {",
-			  "  console.log(\\"url:\\", server.tunnel.url());",
+			  "	console.log(\\"url:\\", server.tunnel.url());",
 			  "});",
 			]
 		`);
