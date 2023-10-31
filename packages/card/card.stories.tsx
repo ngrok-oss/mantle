3bb54674bd16f5d49b429c292708152e8d0bb497
Cody A Price <cody@ngrok.com>
2023-10-31 00:10:45 -0500

    git mv card into top level package dir

--- packages/card/card.stories.tsx
+++ packages/card/card.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Card, CardBody, CardFooter, CardHeader, CardTitle } from "./card";
+import { Card, CardBody, CardFooter, CardHeader, CardTitle } from "./";
 
 const meta = {
 	title: "Card",
