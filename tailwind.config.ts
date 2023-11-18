5988a2419fec7392f5a149761694430de4bcb845
Aaron Shekey <aaron@aaronshekey.com>
2023-11-17 19:27:44 -0600

    Borrow a plugin to target Firefox
    This angers the typescript gods

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,23 @@
 import baseConfig from "./packages/core/tailwind.preset";
+import plugin from "tailwindcss/plugin";
 
 export default {
 	presets: [baseConfig],
 	content: ["./packages/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}"],
+	plugins: [
+		// firefox only modifier https://gist.github.com/samselikoff/b3c5126ee4f4e69e60b0af0aa5bfb2e7
+		plugin(({ addVariant, e, postcss }) => {
+			addVariant("firefox", ({ container, separator }) => {
+				const isFirefoxRule = postcss.atRule({
+					name: "supports",
+					params: "(-moz-appearance:none)",
+				});
+				isFirefoxRule.append(container.nodes);
+				container.append(isFirefoxRule);
+				isFirefoxRule.walkRules((rule) => {
+					rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1).replaceAll("\\", "")}`)}`;
+				});
+			});
+		}),
+	],
 };
