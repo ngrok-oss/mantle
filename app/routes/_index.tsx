d455eea2bd18a9a7e96af0ff104b71c4570a0d2f
Cody A Price <cody@ngrok.com>
2023-11-20 22:03:32 -0600

    adds "prefers-contrast: more" support for system and initial theme load

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import type { MetaFunction } from "@vercel/remix";
 export const meta: MetaFunction = () => {
 	return [
 		{ title: "@ngrok/mantle" },
-		{ name: "description", content: "mantle is ngrok’s UI library and design system" },
+		{ name: "description", content: "mantle is ngrok's UI library and design system" },
 	];
 };
 
@@ -16,14 +16,14 @@ export default function Page() {
 				<a className="text-blue-600 hover:underline" href="https://ngrok.com">
 					ngrok
 				</a>
-				’s UI library and design system that powers its front-end.
+				's UI library and design system that powers its front-end.
 			</p>
 
 			<h2 id="dependencies" className="mt-8 text-3xl font-medium">
 				Dependencies
 			</h2>
 			<p className="mt-3 text-gray-600">
-				Mantle’s styling is composed using{" "}
+				Mantle's styling is composed using{" "}
 				<a className="text-blue-600 hover:underline" href="https://tailwindcss.com">
 					Tailwind
 				</a>
@@ -35,11 +35,11 @@ export default function Page() {
 				<a className="text-blue-600 hover:underline" href="https://ui.shadcn.com">
 					shadcn/ui
 				</a>
-				’s markup and{" "}
+				's markup and{" "}
 				<a className="text-blue-600 hover:underline" href="https://www.radix-ui.com">
 					Radix
 				</a>
-				’s primitives. Its documentation is built in{" "}
+				's primitives. Its documentation is built in{" "}
 				<a className="text-blue-600 hover:underline" href="https://remix.run/">
 					Remix
 				</a>
@@ -50,7 +50,7 @@ export default function Page() {
 				Status
 			</h2>
 			<p className="mt-3 text-gray-600">
-				Mantle is a work in progress that’s currently adding components. It intends to replace new and existing ngrok
+				Mantle is a work in progress that's currently adding components. It intends to replace new and existing ngrok
 				user interfaces.
 			</p>
 		</div>
---
+++ packages/theme-provider/theme-provider.test.tsx
@@ -0,0 +1,25 @@
+import { afterEach, describe, expect, test, vi } from "vitest";
+
+import { determineThemeFromMediaQuery } from ".";
+
+describe("determineThemeFromMediaQuery", () => {
+	test("given prefersDarkMode=true and prefersContrastMore=false, returns dark", () => {
+		expect(determineThemeFromMediaQuery({ prefersDarkMode: true, prefersContrastMore: false })).toBe("dark");
+	});
+
+	test("given prefersDarkMode=false and prefersContrastMore=false, returns light", () => {
+		expect(determineThemeFromMediaQuery({ prefersDarkMode: false, prefersContrastMore: false })).toBe("light");
+	});
+
+	test("given prefersDarkMode=true and prefersContrastMore=true, returns dark-high-contrast", () => {
+		expect(determineThemeFromMediaQuery({ prefersDarkMode: true, prefersContrastMore: true })).toBe(
+			"dark-high-contrast",
+		);
+	});
+
+	test("given prefersDarkMode=false and prefersContrastMore=true, returns light-high-contrast", () => {
+		expect(determineThemeFromMediaQuery({ prefersDarkMode: false, prefersContrastMore: true })).toBe(
+			"light-high-contrast",
+		);
+	});
+});
