c9d10c781ff2038f0907f84752b0061c5046f554
Cody A Price <cody@ngrok.com>
2023-11-22 22:16:31 -0600

    0.0.1-alpha.5

---
+++ rollup.config.mjs
@@ -0,0 +1,59 @@
+import babel from "@rollup/plugin-babel";
+import extensions from "rollup-plugin-extensions";
+import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
+import packageJson from "./package.json" assert { type: "json" };
+
+const { name, version } = packageJson;
+
+const config = [
+	{
+		input: "./components/index.tsx",
+		output: {
+			file: "dist/index.js",
+			format: "esm",
+			sourcemap: true,
+			banner: createBanner(),
+			exports: "named",
+		},
+		external: ["react", "react-dom"],
+		plugins: [
+			extensions({ extensions: [".ts", ".tsx"] }),
+			babel({
+				babelHelpers: "bundled",
+				exclude: "node_modules",
+				presets: [["@babel/preset-env", { loose: true }], "@babel/preset-react", "@babel/preset-typescript"],
+				extensions: [".ts", ".tsx"],
+			}),
+			typescript({
+				tsconfig: "./tsconfig.publish.json",
+				noEmitOnError: true,
+			}),
+			copy({
+				targets: [{ src: ["components/core"], dest: "dist" }],
+			}),
+		],
+	},
+];
+
+export default config;
+
+/**
+ * @typedef {import('rollup').InputOptions} RollupInputOptions
+ * @typedef {import('rollup').OutputOptions} RollupOutputOptions
+ * @typedef {import('rollup').RollupOptions} RollupOptions
+ * @typedef {import('rollup').Plugin} RollupPlugin
+ */
+
+function createBanner() {
+	return `/**
+ * ${name} v${version}
+ *
+ * Copyright (c) ngrok.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */`;
+}
