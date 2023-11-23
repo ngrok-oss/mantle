45252fc7ab2a73e247208860e5d84736d6011ae5
Aaron Shekey <aaron@aaronshekey.com>
2023-11-23 11:12:41 -0600

    A better example

--- packages/code-block/code-block.stories.tsx
+++ packages/code-block/code-block.stories.tsx
@@ -53,17 +53,48 @@ export const WithHeaderFileExpanding: Story = {
 					<CodeBlockCopyButton />
 					<CodeBlockContent language="js">
 						{code`
-							const http = require('http');
-							const ngrok = require("@ngrok/ngrok");
+							const listener = await ngrok.connect({
+								// session configuration
+								addr: \`localhost:8080\`, // or \`8080\` or \`unix:$\{UNIX_SOCKET\}\`
+								authtoken: "<authtoken>",
+								authtoken_from_env: true,
+								on_status_change: (addr, error) => {
+									console.log(\`disconnected, addr $\{addr\} error: $\{error\}\`);
+								},
+								session_metadata: "Online in One Line",
 
-							const server = http.createServer((req, res) => {
-								res.writeHead(${status});
-								res.end(${hello});
-							});
-
-							// Consumes authtoken from env automatically
-							ngrok.listen(server).then(() => {
-								console.log("url:", server.tunnel.url());
+								// listener configuration
+								allow_user_agent: "^mozilla.*",
+								basic_auth: ["ngrok:online1line"],
+								circuit_breaker: 0.1,
+								compression: true,
+								deny_user_agent: "^curl.*",
+								domain: "<domain>",
+								ip_restriction_allow_cidrs: ["0.0.0.0/0"],
+								ip_restriction_deny_cidrs: ["10.1.1.1/32"],
+								metadata: "example listener metadata from nodejs",
+								mutual_tls_cas: [fs.readFileSync('ca.crt', 'utf8')],
+								oauth_provider: "google",
+								oauth_allow_domains: ["<domain>"],
+								oauth_allow_emails: ["<email>"],
+								oauth_scopes: ["<scope>"],
+								oauth_client_id: "<id>",
+								oauth_client_secret: "<secret>",
+								oidc_issuer_url: "<url>",
+								oidc_client_id: "<id>",
+								oidc_client_secret: "<secret>",
+								oidc_allow_domains: ["<domain>"],
+								oidc_allow_emails: ["<email>"],
+								oidc_scopes: ["<scope>"],
+								proxy_proto: "", // One of: "", "1", "2"
+								request_header_remove: ["X-Req-Nope"],
+								response_header_remove: ["X-Res-Nope"],
+								request_header_add: ["X-Req-Yup:true"],
+								response_header_add: ["X-Res-Yup:true"],
+								schemes: ["HTTPS"],
+								verify_webhook_provider: "twilio",
+								verify_webhook_secret: "asdf",
+								websocket_tcp_converter: true,
 							});
 						`}
 					</CodeBlockContent>
