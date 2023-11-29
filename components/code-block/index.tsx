796fb3978807eaa9468d9e3156a0fc5b478c0815
Aaron Shekey <aaron@aaronshekey.com>
2023-11-29 17:50:51 -0600

    Unused shorthand

--- components/code-block/index.tsx
+++ components/code-block/index.tsx
@@ -179,7 +179,7 @@ const CodeBlockCopyButton = forwardRef<HTMLButtonElement, CodeBlockCopyButtonPro
 			<button
 				type="button"
 				className={cx(
-					"absolute right-[0.625rem] top-[0.625rem] z-10 flex h-8 w-8 items-center justify-center place-items-center rounded-sm border border-gray-300 bg-gray-50 shadow-[-1rem_0_0.75rem_-0.375rem_hsl(var(--gray-050)),1rem_0_0_-0.25rem_hsl(var(--gray-050))] hover:border-gray-400 hover:bg-gray-200",
+					"absolute right-[0.625rem] top-[0.625rem] z-10 flex h-8 w-8 items-center justify-center rounded-sm border border-gray-300 bg-gray-50 shadow-[-1rem_0_0.75rem_-0.375rem_hsl(var(--gray-050)),1rem_0_0_-0.25rem_hsl(var(--gray-050))] hover:border-gray-400 hover:bg-gray-200",
 					copied &&
 						"gap-1 w-auto border-transparent bg-green-500 focus:bg-green-500 px-2 text-button hover:border-transparent hover:bg-green-500",
 					className,
