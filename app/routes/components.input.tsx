6c1f57c755902a6366a887f0c0bb66478350b3e5
Aaron Shekey <aaron@aaronshekey.com>
2023-11-29 09:00:43 -0600

    Updated padding

--- app/routes/components.input.tsx
+++ app/routes/components.input.tsx
@@ -15,7 +15,7 @@ export default function Page() {
 		<div>
 			<h1 className="text-5xl font-medium">Input</h1>
 			<p className="mt-4 text-xl text-gray-600">Fundamental component for inputs.</p>
-			<div className="mt-4 flex items-center justify-center rounded-lg rounded-b-none border border-gray-300 bg-background p-9">
+			<div className="mt-4 flex items-center justify-center rounded-lg rounded-b-none border border-gray-300 bg-background p-4 md:p-9">
 				<Input placeholder="Enter a username" />
 			</div>
 
