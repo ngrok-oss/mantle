5651faf0fd6eeffb2907ef0ad119cb2084608402
Aaron Shekey <aaron@aaronshekey.com>
2023-11-29 09:00:30 -0600

    A better code block example

--- app/routes/components.code-block.tsx
+++ app/routes/components.code-block.tsx
@@ -1,4 +1,4 @@
-import { CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton, CodeBlockExpanderButton } from "@/code-block";
+import { CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton, CodeBlockExpanderButton, CodeBlockHeader, CodeBlockTitle } from "@/code-block";
 import { code } from "@/code-block/code";
 import type { MetaFunction } from "@vercel/remix";
 
@@ -9,13 +9,27 @@ export const meta: MetaFunction = () => {
 	];
 };
 
+const FileIcon = () => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
+		<path
+			fillRule="evenodd"
+			d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z"
+			clipRule="evenodd"
+		/>
+	</svg>
+);
+
 export default function Page() {
 	return (
 		<div>
 			<h1 className="text-5xl font-medium">Code Block</h1>
 			<p className="mt-4 text-xl text-gray-600">Code blocks render and apply syntax highlighting to blocks of code.</p>
-			<div className="mt-4 flex items-center justify-center rounded-lg rounded-b-none border border-gray-300 bg-background p-9">
+			<div className="mt-4 flex items-center justify-center rounded-lg rounded-b-none border border-gray-300 bg-background p-4 md:p-9">
 				<CodeBlock>
+					<CodeBlockHeader className="flex items-center gap-1">
+							<FileIcon />
+							<CodeBlockTitle>ngrok-example.js</CodeBlockTitle>
+					</CodeBlockHeader>
 					<CodeBlockBody>
 						<CodeBlockCopyButton />
 						<CodeBlockCode language="js">
@@ -75,52 +89,15 @@ export default function Page() {
 					<CodeBlockCode language="tsx">
 						{code`
 							<CodeBlock>
+								<CodeBlockHeader>
+									<Icon />
+									<CodeBlockTitle>…</CodeBlockTitle>
+								</CodeBlockHeader>
 								<CodeBlockBody>
 									<CodeBlockCopyButton />
-									<CodeBlockCode language="js">
+									<CodeBlockCode language="…">
 										{code\`
-											const listener = await ngrok.connect({
-												// session configuration
-												addr: \`localhost:8080\`, // or \`8080\` or \`unix:$\{UNIX_SOCKET\}\`
-												authtoken: "<authtoken>",
-												authtoken_from_env: true,
-												on_status_change: (addr, error) => {
-													console.log(\`disconnected, addr $\{addr\} error: $\{error\}\`);
-												},
-												session_metadata: "Online in One Line",
-												// listener configuration
-												allow_user_agent: "^mozilla.*",
-												basic_auth: ["ngrok:online1line"],
-												circuit_breaker: 0.1,
-												compression: true,
-												deny_user_agent: "^curl.*",
-												domain: "<domain>",
-												ip_restriction_allow_cidrs: ["0.0.0.0/0"],
-												ip_restriction_deny_cidrs: ["10.1.1.1/32"],
-												metadata: "example listener metadata from nodejs",
-												mutual_tls_cas: [fs.readFileSync('ca.crt', 'utf8')],
-												oauth_provider: "google",
-												oauth_allow_domains: ["<domain>"],
-												oauth_allow_emails: ["<email>"],
-												oauth_scopes: ["<scope>"],
-												oauth_client_id: "<id>",
-												oauth_client_secret: "<secret>",
-												oidc_issuer_url: "<url>",
-												oidc_client_id: "<id>",
-												oidc_client_secret: "<secret>",
-												oidc_allow_domains: ["<domain>"],
-												oidc_allow_emails: ["<email>"],
-												oidc_scopes: ["<scope>"],
-												proxy_proto: "", // One of: "", "1", "2"
-												request_header_remove: ["X-Req-Nope"],
-												response_header_remove: ["X-Res-Nope"],
-												request_header_add: ["X-Req-Yup:true"],
-												response_header_add: ["X-Res-Yup:true"],
-												schemes: ["HTTPS"],
-												verify_webhook_provider: "twilio",
-												verify_webhook_secret: "asdf",
-												websocket_tcp_converter: true,
-											});
+											…
 										\`}
 									</CodeBlockCode>
 									<CodeBlockExpanderButton />
@@ -128,7 +105,6 @@ export default function Page() {
 							</CodeBlock>
 						`}
 					</CodeBlockCode>
-					<CodeBlockExpanderButton />
 				</CodeBlockBody>
 			</CodeBlock>
 		</div>
