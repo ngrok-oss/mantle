ee872e341779593a011ae5392eea3e9177f84606
Cody A Price <cody@ngrok.com>
2023-12-04 11:18:49 -0600

    optimize publishing

---
+++ packages/tailwind-preset/src/index.tsx
@@ -0,0 +1,5 @@
+import { preset } from "./tailwind.preset";
+
+export type { Preset } from "./tailwind.preset";
+
+export default preset;
---
+++ packages/tailwind-preset/src/tailwind-plugin-firefox-variant.ts
@@ -0,0 +1,28 @@
+import type { Postcss, Root } from "postcss";
+import plugin from "tailwindcss/plugin";
+import type { PluginAPI } from "tailwindcss/types/config";
+
+/**
+ * This plugin adds a `firefox` variant to TailwindCSS.
+ *
+ * @see https://gist.github.com/samselikoff/b3c5126ee4f4e69e60b0af0aa5bfb2e7
+ */
+const firefoxVariantPlugin = plugin(
+	// @ts-expect-error PluginAPI is not typed correctly, missing postcss
+	(api: PluginAPI & { postcss: Postcss }) => {
+		// @ts-expect-error addVariant is not typed correctly
+		api.addVariant("firefox", ({ container, separator }: { container: Root; separator: string }) => {
+			const isFirefoxRule = api.postcss.atRule({
+				name: "supports",
+				params: "(-moz-appearance:none)",
+			});
+			isFirefoxRule.append(container.nodes);
+			container.append(isFirefoxRule);
+			isFirefoxRule.walkRules((rule) => {
+				rule.selector = `.${api.e(`firefox${separator}${rule.selector.slice(1).replace(/\\/g, "")}`)}`;
+			});
+		});
+	},
+);
+
+export default firefoxVariantPlugin;
