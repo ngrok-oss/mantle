91d70d4391d39610a55e34dafdfcfc3aefd2008e
Cody A Price <cody@ngrok.com>
2023-12-05 08:20:33 -0600

    use other index

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import preset from "./packages/tailwind-preset";
+import preset from "./packages/tailwind-preset/src/index";
 
 export default {
 	presets: [preset],
