883f3a8780793372d7e25c6e4b643dcb80c7c2dd
Aaron Shekey <aaron@aaronshekey.com>
2023-12-06 14:11:05 -0600

    Remove @radix-ui/react-icons

--- components/drawer/index.tsx
+++ components/drawer/index.tsx
@@ -1,5 +1,5 @@
+import { X } from "@phosphor-icons/react/X";
 import * as DrawerPrimitive from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
 import { cva, type VariantProps } from "class-variance-authority";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef, HTMLAttributes } from "react";
 import { cx } from "../cx";
@@ -56,8 +56,7 @@ const DrawerContent = forwardRef<ElementRef<typeof DrawerPrimitive.Content>, Dra
 			<DrawerPrimitive.Content ref={ref} className={cx(drawerVariants({ side }), className)} {...props}>
 				{children}
 				<DrawerPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-					{/* TODO(cody): replaceme w/ real icon */}
-					<Cross2Icon className="h-4 w-4" />
+					<X className="h-6 w-6" />
 					<span className="sr-only">Close</span>
 				</DrawerPrimitive.Close>
 			</DrawerPrimitive.Content>
