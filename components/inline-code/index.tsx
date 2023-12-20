48b0fc3ddf640a03821abd2c145648ce08f20b70
Aaron Shekey <aaron@aaronshekey.com>
2023-12-20 12:18:19 -0600

    Inline code refinements

--- components/inline-code/index.tsx
+++ components/inline-code/index.tsx
@@ -7,7 +7,10 @@ import { cx } from "../cx";
 const InlineCode = forwardRef<HTMLSpanElement, HTMLAttributes<HTMLSpanElement>>(({ className, ...props }, ref) => (
 	<code
 		ref={ref}
-		className={cx("font-mono bg-gray-100 border border-gray-300 text-[0.8em] rounded-md px-1 py-0.5", className)}
+		className={cx(
+			"font-mono bg-gray-100 border border-gray-300 dark:border-gray-500 dark:bg-gray-200 text-[0.8em] rounded-md px-1 py-0.5",
+			className,
+		)}
 		{...props}
 	/>
 ));
