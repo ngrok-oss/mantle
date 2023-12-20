d0d46a840dff9efa7d0a096a04cb4ba54b6aa214
Aaron Shekey <aaron@aaronshekey.com>
2023-12-20 11:59:35 -0600

    Better example colors

--- app/components/example.tsx
+++ app/components/example.tsx
@@ -8,7 +8,7 @@ export function Example({ children, className, style }: Props) {
 	return (
 		<div
 			className={cx(
-				"rounded-lg border to-gray-100 from-[#fcfcfc] dark:from-gray-200 dark:to-gray-50 bg-gradient-to-br flex items-center justify-center p-4 md:p-16 border-b-0 border-gray-300 rounded-b-none dark-high-contrast:from-gray-50 dark-high-contrast:to-white light-high-contrast:from-white light-high-contrast:to-gray-50",
+				"rounded-lg border to-gray-100 from-[#fcfcfc] dark:from-gray-200 dark:to-gray-50 bg-gradient-to-br flex items-center justify-center p-4 md:p-16 border-b-0 border-gray-300 rounded-b-none dark-high-contrast:from-gray-50 dark-high-contrast:to-white high-contrast:from-white high-contrast:to-gray-50",
 				className,
 			)}
 			style={style}
