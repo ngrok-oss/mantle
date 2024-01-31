cce585131c73db43237cd5a960bb0fc8e26cbd0d
Aaron Shekey <aaron@aaronshekey.com>
2024-01-31 08:46:55 -0600

    Button revisions (#109)
    * Colors
    
    * Additional states
    
    * Focus border refinements
    
    * Use transparency on ghost and outline hover / active
    
    * Get the heights right
    
    * Bump the muted outline border a bit

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -6,14 +6,16 @@ import type { WithAsChild } from "../types/as-child";
 import type { VariantProps } from "../types/variant-props";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center rounded-md font-medium focus-visible:outline-none focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 border whitespace-nowrap",
+	"inline-flex items-center justify-center rounded-md font-medium focus-visible:outline-none focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50 h-11 sm:h-9 px-3 border whitespace-nowrap sm:text-sm",
 	{
 		variants: {
 			appearance: {
-				outline: "border-blue-500 text-blue-600 hover:bg-blue-100 active:bg-blue-200 focus-visible:ring-blue-500/25",
+				outline:
+					"border-blue-600 text-blue-600 hover:bg-blue-600/10 active:bg-blue-600/20 focus-visible:ring-blue-500/25",
 				solid:
-					"border-transparent bg-blue-500 text-button hover:bg-blue-600 active:bg-blue-700 focus-visible:ring-blue-500/25",
-				ghost: "border-transparent text-blue-600 hover:bg-blue-100 active:bg-blue-200 focus-visible:ring-blue-500/25",
+					"border-transparent bg-blue-500 text-button hover:bg-blue-600 active:bg-blue-700 focus-visible:border-blue-600 focus-visible:ring-blue-500/25",
+				ghost:
+					"border-transparent text-blue-600 hover:bg-blue-600/10 active:bg-blue-600/20 focus-visible:ring-blue-500/25",
 			},
 			priority: {
 				default: "",
@@ -28,33 +30,37 @@ const buttonVariants = cva(
 			{
 				appearance: "ghost",
 				priority: "danger",
-				class: "border-transparent text-red-600 hover:bg-red-100 active:bg-red-200 focus-visible:ring-red-500/25",
+				class: "border-transparent text-red-600 hover:bg-red-600/10 active:bg-red-600/20 focus-visible:ring-red-500/25",
 			},
 			{
 				appearance: "outline",
 				priority: "danger",
 				class:
-					"border-transparent border-red-500 text-red-600 hover:bg-red-100 active:bg-red-200 focus-visible:ring-red-500/25",
+					"border-transparent border-red-600 text-red-600 hover:bg-red-600/10 active:bg-red-600/20 focus-visible:ring-red-500/25",
 			},
 			{
 				appearance: "solid",
 				priority: "danger",
-				class: "border-transparent bg-red-500 hover:bg-red-600 active:bg-red-700 focus-visible:ring-red-500/25",
+				class:
+					"border-transparent bg-red-500 hover:bg-red-600 active:bg-red-700 focus-visible:ring-red-500/25 focus-visible:border-red-600",
 			},
 			{
 				appearance: "ghost",
 				priority: "muted",
-				class: "border-transparent text-gray-500 hover:bg-gray-100 active:bg-gray-200 focus-visible:ring-gray-500/25",
+				class:
+					"border-transparent text-gray-900 hover:bg-gray-600/10 active:bg-gray-600/20 focus-visible:ring-blue-500/25",
 			},
 			{
 				appearance: "outline",
 				priority: "muted",
-				class: "border-gray-400 text-gray-500 hover:bg-gray-100 active:bg-gray-200 focus-visible:ring-gray-500/25",
+				class:
+					"border-gray-400 text-gray-900 hover:bg-gray-600/10 active:bg-gray-600/20 focus-visible:ring-blue-500/25 focus-visible:border-blue-600",
 			},
 			{
 				appearance: "solid",
 				priority: "muted",
-				class: "border-transparent bg-gray-400 hover:bg-gray-500 active:bg-gray-600 focus-visible:ring-gray-500/25",
+				class:
+					"border-transparent bg-gray-500 hover:bg-gray-600 active:bg-gray-700 focus-visible:ring-gray-500/25 focus-visible:border-gray-600",
 			},
 		],
 	},
