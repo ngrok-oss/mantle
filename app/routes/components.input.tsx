e5141a468a6209a453ad0c6645920fe941b9d5b2
Aaron Shekey <aaron@aaronshekey.com>
2024-01-31 09:46:23 -0600

    Input styling

--- app/routes/components.input.tsx
+++ app/routes/components.input.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/button";
 import { CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton } from "@/code-block";
 import { code } from "@/code-block/code";
 import { Input } from "@/input";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/table";
 import type { HeadersFunction, MetaFunction } from "@remix-run/node";
 import { Example } from "~/components/example";
 
@@ -23,8 +25,25 @@ export default function Page() {
 			<h1 className="text-5xl font-medium">Input</h1>
 			<p className="mt-4 text-xl text-gray-600">Fundamental component for inputs.</p>
 
-			<Example className="mt-4">
-				<Input placeholder="Enter a username" />
+			<Example className="mt-4 flex-col gap-6">
+				<div className="flex">
+					<Input placeholder="Enter a username" />
+					<Button className="ml-2" priority="default" appearance="outline" type="button">
+						Submit
+					</Button>
+				</div>
+				<div className="flex">
+					<Input placeholder="Enter a username" state="danger" />
+					<Button className="ml-2" priority="danger" appearance="outline" type="button">
+						Submit
+					</Button>
+				</div>
+				<div className="flex">
+					<Input placeholder="Enter a username" disabled />
+					<Button className="ml-2" priority="default" appearance="outline" type="button" disabled>
+						Submit
+					</Button>
+				</div>
 			</Example>
 			<CodeBlock className="rounded-b-lg rounded-t-none">
 				<CodeBlockBody>
@@ -32,6 +51,29 @@ export default function Page() {
 					<CodeBlockCode language="tsx">{code`<Input placeholder="Enter a username" />`}</CodeBlockCode>
 				</CodeBlockBody>
 			</CodeBlock>
+
+			<h2 className="mt-16 text-3xl font-medium">API Reference</h2>
+			<div className="z-10 mt-4 overflow-hidden rounded-lg border border-gray-300">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>Prop</TableHead>
+							<TableHead>Type</TableHead>
+							<TableHead>Default</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody className="font-mono text-xs text-gray-600">
+						<TableRow>
+							<TableCell className="align-top font-medium">state</TableCell>
+							<TableCell className="space-y-2 align-top text-xs">
+								<p>default</p>
+								<p>danger</p>
+							</TableCell>
+							<TableCell className="align-top">default</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</div>
 		</div>
 	);
 }
