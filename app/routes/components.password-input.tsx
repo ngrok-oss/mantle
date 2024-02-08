046ab556a4938453004fbb1dd395b4bb485ed3a3
Cody A Price <cody@ngrok.com>
2024-02-08 00:42:27 -0600

    adds PasswordInput component

---
+++ app/routes/components.password-input.tsx
@@ -0,0 +1,66 @@
+import { code, CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton } from "@/code-block";
+import { PasswordInput } from "@/password-input";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/table";
+import type { HeadersFunction, MetaFunction } from "@remix-run/node";
+import { Example } from "~/components/example";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "@ngrok/mantle — PasswordInput" },
+		{ name: "description", content: "mantle is ngrok's UI library and design system" },
+	];
+};
+
+export const headers: HeadersFunction = () => {
+	return {
+		"Cache-Control": "max-age=300, stale-while-revalidate=604800",
+	};
+};
+
+export default function Page() {
+	return (
+		<div>
+			<h1 className="text-5xl font-medium">Password Input</h1>
+			<p className="mt-4 text-xl text-gray-600">Fundamental component for password inputs.</p>
+
+			<Example className="mt-4 flex-col gap-4">
+				<PasswordInput className="max-w-64" placeholder="Enter a username" />
+				<PasswordInput className="max-w-64" placeholder="Enter a username" aria-invalid />
+			</Example>
+			<CodeBlock className="rounded-b-lg rounded-t-none">
+				<CodeBlockBody>
+					<CodeBlockCopyButton />
+					<CodeBlockCode language="tsx">{code`
+						<>
+							<PasswordInput placeholder="Enter a username" />
+							<PasswordInput placeholder="Enter a username" aria-invalid />
+						</>
+					`}</CodeBlockCode>
+				</CodeBlockBody>
+			</CodeBlock>
+
+			<h2 className="mt-16 text-3xl font-medium">API Reference</h2>
+			<div className="z-10 mt-4 overflow-hidden rounded-lg border border-gray-300">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>Prop</TableHead>
+							<TableHead>Type</TableHead>
+							<TableHead>Default</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody className="font-mono text-xs text-gray-600">
+						{/* <TableRow>
+							<TableCell className="align-top font-medium">state</TableCell>
+							<TableCell className="space-y-2 align-top text-xs">
+								<p>default</p>
+								<p>danger</p>
+							</TableCell>
+							<TableCell className="align-top">default</TableCell>
+						</TableRow> */}
+					</TableBody>
+				</Table>
+			</div>
+		</div>
+	);
+}
