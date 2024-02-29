108b2d4874cf413037b4da8f5177bd6cfc9d0c87
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2024-02-29 15:23:46 -0600

    adds data-drag-over and styles for dragging over TextArea (#161)
    * adds data-drag-over and styles for dragging over TextArea
    
    * increment patch versions
    
    * adds ring-4 to dragging style

--- components/text-area/src/text-area.tsx
+++ components/text-area/src/text-area.tsx
@@ -1,16 +1,18 @@
 import { cva } from "class-variance-authority";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import type { TextareaHTMLAttributes } from "react";
 import { cx } from "../../core";
 import { VariantProps } from "../../types/";
 
 const textAreaVariants = cva(
-	"border-input flex min-h-[5.75rem] w-full rounded-md border bg-white px-3 py-[calc(theme(spacing[2.5])-1px)] shadow-sm focus-visible:outline-none focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 sm:min-h-[4.75rem] sm:py-[calc(theme(spacing[2])-1px)] sm:text-sm",
+	"border-input flex min-h-[5.75rem] w-full rounded-md border bg-white px-3 py-[calc(theme(spacing[2.5])-1px)] shadow-sm focus-visible:outline-none focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 sm:min-h-[4.75rem] sm:py-[calc(theme(spacing[2])-1px)] sm:text-sm data-drag-over:border-dashed data-drag-over:ring-4",
 	{
 		variants: {
 			state: {
-				default: "text-strong border-form placeholder:text-placeholder focus:border-accent focus:ring-accent",
-				danger: "border-danger placeholder:text-placeholder focus:border-danger focus:ring-danger",
+				default:
+					"text-strong border-form placeholder:text-placeholder focus:border-accent ring-accent data-drag-over:border-accent",
+				danger:
+					"border-danger placeholder:text-placeholder focus:border-danger ring-danger data-drag-over:border-danger data-drag-over:border-dashed",
 			},
 			appearance: {
 				monospaced: "font-mono text-[0.9375rem] sm:text-[0.8125rem]",
@@ -28,7 +30,28 @@ export type TextAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & Pick<T
 
 const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({ appearance, className, ...props }, ref) => {
 	const state = props["aria-invalid"] ? "danger" : "default";
-	return <textarea className={cx(textAreaVariants({ appearance, state }), className)} ref={ref} {...props} />;
+	const [isDragOver, setIsDragOver] = useState(false);
+
+	return (
+		<textarea
+			onDrop={(event) => {
+				setIsDragOver(false);
+				props.onDrop?.(event);
+			}}
+			onDragEnter={(event) => {
+				setIsDragOver(true);
+				props.onDragEnter?.(event);
+			}}
+			onDragLeave={(event) => {
+				setIsDragOver(false);
+				props.onDragLeave?.(event);
+			}}
+			data-drag-over={isDragOver}
+			className={cx(textAreaVariants({ appearance, state }), className)}
+			ref={ref}
+			{...props}
+		/>
+	);
 });
 TextArea.displayName = "TextArea";
 
