56afc9cf900069253c1a71b75ae967428606ced9
Micah Wolfe <micah@micahwolfe.co>
2024-03-11 10:40:11 -0700

    prettier

--- components/button/src/button.tsx
+++ components/button/src/button.tsx
@@ -25,13 +25,12 @@ const buttonVariants = cva(
 			 * Defines the visual style of the Button.
 			 */
 			appearance: {
-				
 				filled:
 					"inline-flex h-11 border border-transparent bg-filled-accent px-3 font-medium text-on-filled hover:bg-filled-accent-hover focus-visible:border-accent-600 focus-visible:ring-focus-accent active:bg-filled-accent-active sm:h-9",
 				ghost:
 					"inline-flex h-11 border border-transparent px-3 font-medium text-accent-600 hover:bg-accent-500/10 hover:text-accent-700 focus-visible:ring-focus-accent active:bg-accent-500/15 active:text-accent-700 sm:h-9",
 				outlined:
-					"inline-flex h-11 border border-accent-600 hover:border-accent-700 active:border-accent-700 bg-form px-3 font-medium text-accent-600 hover:text-accent-700 active:text-accent-700 hover:bg-accent-50 focus-visible:ring-focus-accent active:bg-accent-100 sm:h-9",
+					"inline-flex h-11 border border-accent-600 bg-form px-3 font-medium text-accent-600 hover:border-accent-700 hover:bg-accent-50 hover:text-accent-700 focus-visible:ring-focus-accent active:border-accent-700 active:bg-accent-100 active:text-accent-700 sm:h-9",
 				link: "group inline cursor-pointer border-transparent text-accent-600 hover:underline focus-visible:ring-focus-accent",
 			},
 			/**
@@ -78,7 +77,7 @@ const buttonVariants = cva(
 				appearance: "outlined",
 				priority: "danger",
 				class:
-					"border-danger-600 hover:border-danger-700 active:border-danger-700 bg-form text-danger-600 hover:text-danger-700 active:text-danger-700 hover:bg-danger-50 focus-visible:ring-focus-danger active:bg-danger-100",
+					"border-danger-600 bg-form text-danger-600 hover:border-danger-700 hover:bg-danger-50 hover:text-danger-700 focus-visible:ring-focus-danger active:border-danger-700 active:bg-danger-100 active:text-danger-700",
 			},
 			{
 				appearance: "filled",
@@ -95,13 +94,13 @@ const buttonVariants = cva(
 				appearance: "ghost",
 				priority: "neutral",
 				class:
-					"text-strong border-transparent hover:text-strong hover:bg-neutral-500/10 active:text-strong focus-visible:ring-focus-accent active:bg-neutral-500/15",
+					"border-transparent text-strong hover:bg-neutral-500/10 hover:text-strong focus-visible:ring-focus-accent active:bg-neutral-500/15 active:text-strong",
 			},
 			{
 				appearance: "outlined",
 				priority: "neutral",
 				class:
-					"text-strong border-form bg-form hover:bg-form-hover focus-visible:border-accent-600 focus-visible:active:border-accent-600 hover:border-neutral-400 active:border-neutral-400 hover:text-strong active:text-strong focus-visible:ring-focus-accent active:bg-form-active",
+					"border-form bg-form text-strong hover:border-neutral-400 hover:bg-form-hover hover:text-strong focus-visible:border-accent-600 focus-visible:ring-focus-accent active:border-neutral-400 active:bg-form-active active:text-strong focus-visible:active:border-accent-600",
 			},
 			{
 				appearance: "filled",
