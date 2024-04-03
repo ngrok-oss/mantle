5388c4b610b5bcd08cc2fb4ba17072352d165fc4
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2024-04-02 19:54:31 -0500

    CodeBlock: loads all supported languages (#194)

--- components/code-block/src/code-block.tsx
+++ components/code-block/src/code-block.tsx
@@ -13,8 +13,21 @@ import {
 	useState,
 } from "react";
 import "prismjs/components/prism-bash.js";
+import "prismjs/components/prism-bash.js";
+import "prismjs/components/prism-csharp.js";
+import "prismjs/components/prism-css.js";
+import "prismjs/components/prism-go.js";
+import "prismjs/components/prism-java.js";
+import "prismjs/components/prism-javascript.js";
+import "prismjs/components/prism-json.js";
 import "prismjs/components/prism-jsx.js";
+import "prismjs/components/prism-markup.js";
+import "prismjs/components/prism-python.js";
+import "prismjs/components/prism-ruby.js";
+import "prismjs/components/prism-rust.js";
 import "prismjs/components/prism-tsx.js";
+import "prismjs/components/prism-typescript.js";
+import "prismjs/components/prism-yaml.js";
 import { CaretDown } from "@phosphor-icons/react/CaretDown";
 import { Check } from "@phosphor-icons/react/Check";
 import { Copy } from "@phosphor-icons/react/Copy";
