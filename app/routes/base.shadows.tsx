977e8e00ce55f0cde66fc47fa1f74fc222604c36
Aaron Shekey <aaron@aaronshekey.com>
2024-04-05 16:15:33 -0500

    Further shadow specification and refinement (#202)
    * Further shadow specification and refinement
    
    * Page title

--- app/routes/base.shadows.tsx
+++ app/routes/base.shadows.tsx
@@ -2,7 +2,7 @@ import type { HeadersFunction, MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
 	return [
-		{ title: "@ngrok/mantle — Typography" },
+		{ title: "@ngrok/mantle — Shadows" },
 		{ name: "description", content: "mantle is ngrok's UI library and design system" },
 	];
 };
@@ -19,6 +19,9 @@ export default function Page() {
 			<h1 className="text-5xl font-medium">Shadows</h1>
 			<p className="mt-4 text-xl text-body">Tokens for defining elevations.</p>
 			<div className="mt-8 flex gap-8">
+				<div className="size-24 rounded-lg bg-card shadow-inner"></div>
+				<div className="size-24 rounded-lg bg-card shadow-sm"></div>
+				<div className="size-24 rounded-lg bg-card shadow"></div>
 				<div className="size-24 rounded-lg bg-card shadow-md"></div>
 				<div className="size-24 rounded-lg bg-card shadow-lg"></div>
 				<div className="size-24 rounded-lg bg-card shadow-xl"></div>
