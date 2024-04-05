9a27e5fb4aba6300a715a7a6363f6b105201ef94
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2024-04-05 00:38:44 -0500

    alert: improve documentation and intellisense (#199)

--- components/alert/src/alert.tsx
+++ components/alert/src/alert.tsx
@@ -6,6 +6,11 @@ import type { VariantProps } from "../../types";
 
 const alertVariants = cva("relative flex w-full gap-1.5 rounded-md border p-2.5 text-sm", {
 	variants: {
+		/**
+		 * The priority of the Alert. Indicates the importance or impact level of the Alert,
+		 * affecting its color and styling to communicate its purpose to the user.
+		 * @default "default"
+		 */
 		priority: {
 			danger: "border-danger-500/50 bg-danger-500/10 text-danger-700",
 			default: "border-neutral-500/50 bg-neutral-500/10 text-neutral-700",
@@ -21,23 +26,35 @@ const alertVariants = cva("relative flex w-full gap-1.5 rounded-md border p-2.5 
 
 type AlertVariants = VariantProps<typeof alertVariants>;
 
+/**
+ * Displays a callout for user attention.
+ */
 const Alert = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement> & AlertVariants>(
-	({ className, priority, ...props }, ref) => (
+	({ className, priority = "default", ...props }, ref) => (
 		<div ref={ref} className={cx(alertVariants({ priority }), className)} {...props} />
 	),
 );
 Alert.displayName = "Alert";
 
+/**
+ * The container for the content slot of an alert. Place the title and description as direct children.
+ */
 const AlertContent = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
 	<div ref={ref} className={cx("min-w-0 flex-1", className)} {...props} />
 ));
 AlertContent.displayName = "AlertContent";
 
+/**
+ * The title of an alert.
+ */
 const AlertTitle = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLHeadingElement>>(
 	({ className, ...props }, ref) => <h5 ref={ref} className={cx("font-medium", className)} {...props} />,
 );
 AlertTitle.displayName = "AlertTitle";
 
+/**
+ * The description of an alert.
+ */
 const AlertDescription = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(
 	({ className, ...props }, ref) => <div ref={ref} className={cx("text-sm", className)} {...props} />,
 );
