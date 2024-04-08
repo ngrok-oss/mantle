ed4aeae6010768ae4c156679b69406ef77346c17
Cody A Price <cody@ngrok.com>
2024-04-08 13:16:07 -0500

    update docs tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,5 +3,5 @@ import { mantlePreset } from "./packages/tailwind-preset/src/tailwind.preset";
 
 export default {
 	presets: [mantlePreset],
-	content: ["./components/**/*.{ts,tsx,js,jsx}", "./app/**/*.{ts,tsx,js,jsx}"],
+	content: ["./packages/**/*.{ts,tsx,js,jsx}", "./app/**/*.{ts,tsx,js,jsx}"],
 } satisfies Config;
