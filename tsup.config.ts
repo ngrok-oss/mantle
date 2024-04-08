1fba541a131ebd1acc8cf47a5c42b835a4d560ce
Cody A Price <cody@ngrok.com>
2024-04-08 11:59:50 -0500

    fully switch over to tsup from parcel, merge tailwind-preset into main mantle package

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,31 +1,20 @@
 import fs from "fs";
 import { defineConfig } from "tsup";
 
-// read only the directories in the packages folder
-// const packages = fs.readdirSync("packages", { withFileTypes: true }).filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
-
-// read all of the folders in the packages folder, filter out tailwind-preset and then map them to the tsup config
+// read all of the folders in the packages folder and then map them to the tsup config
 const packages = fs
 	.readdirSync("packages", { withFileTypes: true })
 	.filter((dirent) => dirent.isDirectory())
 	.map((dirent) => dirent.name)
-	.filter((name) => name !== "tailwind-preset")
-	.reduce(
-		(acc, name) => {
-			acc[name] = `./packages/${name}/index.tsx`;
-			return acc;
-		},
-		{} as Record<string, string>,
-	);
+	.reduce<Record<string, string>>((acc, name) => {
+		acc[name] = `./packages/${name}/index.tsx`;
+		return acc;
+	}, {});
 
 export default defineConfig((options) => ({
-	// entry: ["./components/index.tsx"],
 	entry: packages,
 	format: ["esm"],
 	dts: true,
 	external: ["react", "react-dom"],
-	// banner: {
-	// 	js: "'use client'",
-	// },
 	...options,
 }));
