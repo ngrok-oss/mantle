f7ce9da42ea4e2332f8e0d1b4806da0b4ff45d43
Cody A Price <cody@ngrok.com>
2024-04-08 12:58:47 -0500

    comment tsup strat

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from "tsup";
 
 const doNotPublish = new Set(["back-to-top-button", "portal"]);
 
-// read all of the folders in the packages folder and then map them to the tsup config
-const packages = fs
+// read all of the directories in the packages directory
+const allPackageDirectories = fs
 	.readdirSync("packages", { withFileTypes: true })
 	.filter((dirent) => dirent.isDirectory())
-	.map((dirent) => dirent.name)
+	.map((dirent) => dirent.name);
+
+// filter out packages that exist in the `doNotPublish` set and then map them to the tsup config entries object
+const packages = allPackageDirectories
 	.filter((name) => !doNotPublish.has(name))
 	.reduce<Record<string, string>>((acc, name) => {
 		acc[name] = `./packages/${name}/index.ts`;
