960fb771f1873e96aef253689bc4cb070f72bfb1
Cody A Price <cody@ngrok.com>
2024-04-28 14:57:29 -0500

    adds api docs for badge

--- app/routes/components.preview.badge.tsx
+++ app/routes/components.preview.badge.tsx
@@ -1,3 +1,4 @@
+import { Anchor } from "@/anchor";
 import { Badge } from "@/badge";
 import { CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton, fmtCode } from "@/code-block";
 import { colors } from "@/color";
@@ -5,6 +6,7 @@ import { InlineCode } from "@/inline-code";
 import { GlobeHemisphereWest } from "@phosphor-icons/react/GlobeHemisphereWest";
 import type { HeadersFunction, MetaFunction } from "@remix-run/node";
 import { Example } from "~/components/example";
+import { Link } from "~/components/link";
 import { PreviewBadge } from "~/components/preview-badge";
 import {
 	PropDefaultValueCell,
@@ -13,6 +15,7 @@ import {
 	PropRow,
 	PropsTable,
 	PropTypeCell,
+	ReactNodePropType,
 	StringPropType,
 } from "~/components/props-table";
 
@@ -79,38 +82,62 @@ export default function Page() {
 
 			<section className="mt-16 space-y-4">
 				<h2 id="api" className="text-3xl font-medium">
-					API Reference TODO(cody): fixme (copypasta from Alert)
+					API Reference
 				</h2>
+				<p className="text-xl text-body">
+					The <InlineCode>Badge</InlineCode> accepts the following props in addition to the{" "}
+					<Anchor href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/span">
+						standard HTML span attributes
+					</Anchor>
+					.
+				</p>
 				<PropsTable>
 					<PropRow>
-						<PropNameCell name="priority" optional />
+						<PropNameCell name="appearance" />
 						<PropTypeCell>
 							<ul>
 								<li>
-									<StringPropType value="danger" />
-								</li>
-								<li>
-									<StringPropType value="default" />
-								</li>
-								<li>
-									<StringPropType value="info" />
-								</li>
-								<li>
-									<StringPropType value="success" />
-								</li>
-								<li>
-									<StringPropType value="warning" />
+									<StringPropType value="muted" />
 								</li>
 							</ul>
 						</PropTypeCell>
+						<PropDefaultValueCell />
+						<PropDescriptionCell>
+							<p>
+								Defines the visual style of the <InlineCode>Badge</InlineCode>. Currently only supports the{" "}
+								<InlineCode>muted</InlineCode> variant.
+							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="color" optional />
+						<PropTypeCell>
+							<ul>
+								{colors.map((color) => (
+									<li key={color}>
+										<StringPropType value={color} />
+									</li>
+								))}
+							</ul>
+						</PropTypeCell>
 						<PropDefaultValueCell>
-							<StringPropType value="outlined" />
+							<StringPropType value="neutral" />
 						</PropDefaultValueCell>
 						<PropDescriptionCell>
 							<p>
-								Indicates the importance or impact level of the <InlineCode>Alert</InlineCode>, affecting its color and
-								styling to communicate its purpose to the user.
+								The color variant of the <InlineCode>Badge</InlineCode>. Supports all{" "}
+								<Link to="/base/colors">named colors</Link>, both functional and from the color palette.
 							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="icon" optional />
+						<PropTypeCell>
+							<ReactNodePropType />
+						</PropTypeCell>
+						<PropDefaultValueCell />
+						<PropDescriptionCell>
+							An icon to render inside the badge. Will be automatically sized for you.
 						</PropDescriptionCell>
 					</PropRow>
 				</PropsTable>
