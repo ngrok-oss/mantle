6d94c5f2ce716a1689e80683ab271f08f2aabbdc
Cody A Price <cody@ngrok.com>
2024-05-01 10:48:44 -0500

    DropdownMenu: switch implementation back to radix (from ariakit)

--- packages/dropdown-menu/src/dropdown-menu.tsx
+++ packages/dropdown-menu/src/dropdown-menu.tsx
@@ -1,57 +1,96 @@
-import * as Ariakit from "@ariakit/react";
+import { CaretRight } from "@phosphor-icons/react/CaretRight";
 import { Check } from "@phosphor-icons/react/Check";
-import type { ComponentPropsWithoutRef, ElementRef, HTMLAttributes } from "react";
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+import type { ComponentPropsWithoutRef, ElementRef } from "react";
 import { forwardRef } from "react";
 import { cx } from "../../cx";
-import { Separator } from "../../separator";
-import type { WithAsChild } from "../../types";
 
-const DropdownMenu = Ariakit.MenuProvider;
+const DropdownMenu = DropdownMenuPrimitive.Root;
 
-type DropdownMenuTriggerProps = Omit<ComponentPropsWithoutRef<typeof Ariakit.MenuButton>, "render"> & WithAsChild;
-const DropdownMenuTrigger = forwardRef<ElementRef<"button">, DropdownMenuTriggerProps>(
-	({ asChild = false, children, ...props }, ref) => {
-		if (asChild) {
-			return (
-				<Ariakit.MenuButton
-					ref={ref}
-					render={typeof children === "object" && children != null ? (children as React.JSX.Element) : <button />}
-				/>
-			);
-		}
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
 
-		return (
-			<Ariakit.MenuButton ref={ref} {...props}>
-				{children}
-			</Ariakit.MenuButton>
-		);
-	},
-);
-DropdownMenuTrigger.displayName = "DropdownMenuTrigger";
+const DropdownMenuGroup = DropdownMenuPrimitive.Group;
 
-const DropdownMenuContent = forwardRef<ElementRef<typeof Ariakit.Menu>, ComponentPropsWithoutRef<typeof Ariakit.Menu>>(
-	({ className, gutter = 8, ...props }, ref) => (
-		<Ariakit.Menu
-			gutter={gutter}
+const DropdownMenuPortal = DropdownMenuPrimitive.Portal;
+
+const DropdownMenuSub = DropdownMenuPrimitive.Sub;
+
+const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+
+const DropdownMenuSubTrigger = forwardRef<
+	ElementRef<typeof DropdownMenuPrimitive.SubTrigger>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubTrigger> & {
+		inset?: boolean;
+	}
+>(({ className, inset, children, ...props }, ref) => (
+	<DropdownMenuPrimitive.SubTrigger
+		className={cx(
+			"focus:bg-accent data-[state=open]:bg-accent relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-9 text-sm outline-none",
+			"data-state-open:bg-popover-hover data-highlighted:bg-popover-hover",
+			"[&>svg]:size-5 [&_svg]:shrink-0",
+			inset && "pl-8",
+			className,
+		)}
+		ref={ref}
+		{...props}
+	>
+		{children}
+		<span className="absolute right-2 flex items-center">
+			<CaretRight className="size-4 shrink-0" weight="bold" />
+		</span>
+	</DropdownMenuPrimitive.SubTrigger>
+));
+DropdownMenuSubTrigger.displayName = "DropdownMenuSubTrigger";
+
+const DropdownMenuSubContent = forwardRef<
+	ElementRef<typeof DropdownMenuPrimitive.SubContent>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent>
+>(({ className, loop = true, ...props }, ref) => (
+	<DropdownMenuPrimitive.SubContent
+		className={cx(
+			"text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+			"my-2 max-h-[calc(var(--radix-dropdown-menu-content-available-height)_-_16px)] overflow-auto",
+			className,
+		)}
+		loop={loop}
+		ref={ref}
+		{...props}
+	/>
+));
+DropdownMenuSubContent.displayName = "DropdownMenuSubContent";
+
+const DropdownMenuContent = forwardRef<
+	ElementRef<typeof DropdownMenuPrimitive.Content>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+>(({ className, loop = true, ...props }, ref) => (
+	<DropdownMenuPrimitive.Portal>
+		<DropdownMenuPrimitive.Content
 			ref={ref}
 			className={cx(
-				"z-50 min-w-[8rem] overflow-hidden rounded border border-popover bg-popover p-1.25 shadow-xl outline-none data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95",
+				"text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded border border-popover bg-popover p-1.25 shadow-xl outline-none",
+				"data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95",
+				"my-2 max-h-[calc(var(--radix-dropdown-menu-content-available-height)_-_16px)] overflow-auto",
 				className,
 			)}
+			loop={loop}
 			{...props}
 		/>
-	),
-);
+	</DropdownMenuPrimitive.Portal>
+));
 DropdownMenuContent.displayName = "DropdownMenuContent";
 
 const DropdownMenuItem = forwardRef<
-	ElementRef<typeof Ariakit.MenuItem>,
-	ComponentPropsWithoutRef<typeof Ariakit.MenuItem>
->(({ className, ...props }, ref) => (
-	<Ariakit.MenuItem
+	ElementRef<typeof DropdownMenuPrimitive.Item>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
+		inset?: boolean;
+	}
+>(({ className, inset, ...props }, ref) => (
+	<DropdownMenuPrimitive.Item
 		ref={ref}
 		className={cx(
-			"relative flex cursor-pointer select-none items-center rounded px-2 py-1.5 text-sm font-normal text-strong outline-none data-active-item:bg-popover-hover data-disabled:pointer-events-none data-disabled:opacity-50 data-active-item:dark:bg-popover-hover",
+			"focus:bg-accent focus:text-accent-foreground relative flex cursor-default select-none items-center rounded px-2 py-1.5 text-sm font-normal outline-none transition-colors data-disabled:pointer-events-none data-disabled:opacity-50 data-highlighted:bg-popover-hover data-active-item:dark:bg-popover-hover",
+			"[&>svg]:size-5 [&_svg]:shrink-0",
+			inset && "pl-8",
 			className,
 		)}
 		{...props}
@@ -59,62 +98,81 @@ const DropdownMenuItem = forwardRef<
 ));
 DropdownMenuItem.displayName = "DropdownMenuItem";
 
-type DropdownMenuCheckboxItemProps = Omit<ComponentPropsWithoutRef<typeof Ariakit.MenuItemCheckbox>, "render">;
-const DropdownMenuCheckboxItem = forwardRef<ElementRef<typeof Ariakit.MenuItemRadio>, DropdownMenuCheckboxItemProps>(
-	({ className, children, ...props }, ref) => (
-		<Ariakit.MenuItemCheckbox
-			ref={ref}
-			className={cx(
-				"relative flex cursor-pointer select-none items-center gap-2 rounded py-1.5 pl-2 pr-9 text-sm font-normal text-strong outline-none data-disabled:pointer-events-none data-disabled:opacity-50",
-				"data-active-item:bg-popover-hover data-active-item:dark:bg-popover-hover",
-				"aria-checked:!bg-filled-accent aria-checked:font-medium aria-checked:text-on-filled",
-				className,
-			)}
-			{...props}
-		>
-			{children}
-			<DropdownMenuItemCheck className="absolute right-2 flex items-center" />
-		</Ariakit.MenuItemCheckbox>
-	),
-);
+const DropdownMenuCheckboxItem = forwardRef<
+	ElementRef<typeof DropdownMenuPrimitive.CheckboxItem>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
+>(({ className, children, checked, ...props }, ref) => (
+	<DropdownMenuPrimitive.CheckboxItem
+		ref={ref}
+		className={cx(
+			"relative flex cursor-pointer select-none items-center gap-2 rounded py-1.5 pl-2 pr-9 text-sm font-normal text-strong outline-none data-disabled:pointer-events-none data-disabled:opacity-50",
+			"data-highlighted:bg-popover-hover data-highlighted:dark:bg-popover-hover",
+			"aria-checked:!bg-filled-accent aria-checked:font-medium aria-checked:text-on-filled",
+			"[&>svg]:size-5 [&_svg]:shrink-0",
+			className,
+		)}
+		checked={checked}
+		{...props}
+	>
+		<span className="absolute right-2 flex items-center">
+			<DropdownMenuPrimitive.ItemIndicator>
+				<Check className="size-4 shrink-0" weight="bold" />
+			</DropdownMenuPrimitive.ItemIndicator>
+		</span>
+		{children}
+	</DropdownMenuPrimitive.CheckboxItem>
+));
 DropdownMenuCheckboxItem.displayName = "DropdownMenuCheckboxItem";
 
-type DropdownMenuRadioItemProps = Omit<ComponentPropsWithoutRef<typeof Ariakit.MenuItemRadio>, "render">;
-const DropdownMenuRadioItem = forwardRef<ElementRef<typeof Ariakit.MenuItemRadio>, DropdownMenuRadioItemProps>(
+type DropdownMenuRadioItemProps = ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.RadioItem> & {
+	name?: string;
+	id?: string;
+};
+
+const DropdownMenuRadioItem = forwardRef<ElementRef<"input">, DropdownMenuRadioItemProps>(
 	({ className, children, ...props }, ref) => (
-		<Ariakit.MenuItemRadio
-			ref={ref}
+		<DropdownMenuPrimitive.RadioItem
 			className={cx(
 				"relative flex cursor-pointer select-none items-center gap-2 rounded py-1.5 pl-2 pr-9 text-sm font-normal text-strong outline-none data-disabled:pointer-events-none data-disabled:opacity-50",
-				"data-active-item:bg-popover-hover data-active-item:dark:bg-popover-hover",
+				"data-highlighted:bg-popover-hover data-highlighted:dark:bg-popover-hover",
 				"aria-checked:!bg-filled-accent aria-checked:font-medium aria-checked:text-on-filled",
+				"[&>svg]:size-5 [&_svg]:shrink-0",
 				className,
 			)}
+			ref={ref}
 			{...props}
 		>
+			<span className="absolute right-2 flex items-center">
+				<DropdownMenuPrimitive.ItemIndicator>
+					<Check className="size-4 shrink-0" weight="bold" />
+				</DropdownMenuPrimitive.ItemIndicator>
+			</span>
 			{children}
-			<DropdownMenuItemCheck className="absolute right-2 flex items-center" />
-		</Ariakit.MenuItemRadio>
+		</DropdownMenuPrimitive.RadioItem>
 	),
 );
 DropdownMenuRadioItem.displayName = "DropdownMenuRadioItem";
 
-const DropdownMenuItemCheck = (props: Omit<HTMLAttributes<HTMLSpanElement>, "children">) => (
-	<Ariakit.MenuItemCheck {...props}>
-		<Check className="size-5" weight="bold" />
-	</Ariakit.MenuItemCheck>
-);
-
-const DropdownMenuLabel = forwardRef<ElementRef<"div">, ComponentPropsWithoutRef<"div">>(
-	({ className, ...props }, ref) => (
-		<div ref={ref} className={cx("px-2 py-1.5 text-sm font-medium text-strong", className)} {...props} />
-	),
-);
+const DropdownMenuLabel = forwardRef<
+	ElementRef<typeof DropdownMenuPrimitive.Label>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
+		inset?: boolean;
+	}
+>(({ className, inset, ...props }, ref) => (
+	<DropdownMenuPrimitive.Label
+		ref={ref}
+		className={cx("px-2 py-1.5 text-sm font-semibold", inset && "pl-8", className)}
+		{...props}
+	/>
+));
 DropdownMenuLabel.displayName = "DropdownMenuLabel";
 
-const DropdownMenuSeparator = forwardRef<ElementRef<typeof Separator>, ComponentPropsWithoutRef<typeof Separator>>(
-	({ className, ...props }, ref) => <Separator ref={ref} className={cx("-mx-2 my-1 w-auto", className)} {...props} />,
-);
+const DropdownMenuSeparator = forwardRef<
+	ElementRef<typeof DropdownMenuPrimitive.Separator>,
+	ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+	<DropdownMenuPrimitive.Separator ref={ref} className={cx("bg-muted -mx-1 my-1 h-px", className)} {...props} />
+));
 DropdownMenuSeparator.displayName = "DropdownMenuSeparator";
 
 const DropdownMenuShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
@@ -124,20 +182,18 @@ DropdownMenuShortcut.displayName = "DropdownMenuShortcut";
 
 export {
 	DropdownMenu,
-	DropdownMenuTrigger,
-	DropdownMenuContent,
-	DropdownMenuItem,
 	DropdownMenuCheckboxItem,
-	DropdownMenuRadioItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
 	DropdownMenuLabel,
+	DropdownMenuPortal,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
 	DropdownMenuSeparator,
 	DropdownMenuShortcut,
-
-	// TODO(cody): audit / add in these exports in a follow up
-	// DropdownMenuGroup,
-	// DropdownMenuPortal,
-	// DropdownMenuSub,
-	// DropdownMenuSubContent,
-	// DropdownMenuSubTrigger,
-	// DropdownMenuRadioGroup,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
 };
