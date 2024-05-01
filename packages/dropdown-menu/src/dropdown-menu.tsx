23bb3651611e8f67bebf9c3b84af7374b67e0674
Cody A Price <cody@ngrok.com>
2024-05-01 12:58:21 -0500

    fix sub content styling, add in .scrollbar to both sub and main content

--- packages/dropdown-menu/src/dropdown-menu.tsx
+++ packages/dropdown-menu/src/dropdown-menu.tsx
@@ -26,7 +26,7 @@ const DropdownMenuSubTrigger = forwardRef<
 >(({ className, inset, children, ...props }, ref) => (
 	<DropdownMenuPrimitive.SubTrigger
 		className={cx(
-			"focus:bg-accent data-[state=open]:bg-accent relative flex cursor-pointer select-none items-center rounded-sm py-1.5 pl-2 pr-9 text-base outline-none sm:text-sm",
+			"focus:bg-accent data-state-open:bg-accent relative flex cursor-pointer select-none items-center rounded-sm py-1.5 pl-2 pr-9 text-base outline-none sm:text-sm",
 			"data-state-open:bg-popover-hover data-highlighted:bg-popover-hover",
 			"[&>svg]:size-6 [&>svg]:sm:size-5 [&_svg]:shrink-0",
 			inset && "pl-8",
@@ -50,7 +50,8 @@ const DropdownMenuSubContent = forwardRef<
 	<DropdownMenuPortal>
 		<DropdownMenuPrimitive.SubContent
 			className={cx(
-				"text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+				"scrollbar",
+				"text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded border border-popover bg-popover p-1.25 shadow-xl data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
 				"my-2 max-h-[calc(var(--radix-dropdown-menu-content-available-height)_-_16px)] overflow-auto",
 				className,
 			)}
@@ -70,6 +71,7 @@ const DropdownMenuContent = forwardRef<
 		<DropdownMenuPrimitive.Content
 			ref={ref}
 			className={cx(
+				"scrollbar",
 				"text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded border border-popover bg-popover p-1.25 shadow-xl outline-none",
 				"data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95",
 				"my-2 max-h-[calc(var(--radix-dropdown-menu-content-available-height)_-_16px)] overflow-auto",
