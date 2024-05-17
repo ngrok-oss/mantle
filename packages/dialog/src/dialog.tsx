73d1c4aefee7b4a14d5ef67469aebdf0a5b72f16
Cody A Price <cody@ngrok.com>
2024-05-17 10:03:24 -0500

    dialog: fixes DialogContent styles (removes overflow + scroll)

--- packages/dialog/src/dialog.tsx
+++ packages/dialog/src/dialog.tsx
@@ -20,7 +20,7 @@ const DialogOverlay = forwardRef<
 	<DialogPrimitive.Overlay
 		ref={ref}
 		className={cx(
-			"fixed inset-0 z-50 bg-overlay backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+			"fixed inset-0 z-50 bg-overlay backdrop-blur-sm data-state-closed:animate-out data-state-closed:fade-out-0 data-state-open:animate-in data-state-open:fade-in-0",
 			className,
 		)}
 		{...props}
@@ -37,7 +37,8 @@ const DialogContent = forwardRef<
 		<DialogPrimitive.Content
 			ref={ref}
 			className={cx(
-				"scrollbar fixed left-[50%] top-[50%] z-50 flex max-h-[calc(100dvh_-_32px)] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] flex-col overflow-y-auto rounded-xl border border-dialog bg-dialog shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+				"fixed left-1/2 top-1/2 z-50 flex max-h-[calc(100dvh_-_32px)] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 flex-col rounded-xl border border-dialog bg-dialog shadow-lg duration-200",
+				"data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-closed:slide-out-to-left-1/2 data-state-closed:slide-out-to-top-[48%] data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95 data-state-open:slide-in-from-left-1/2 data-state-open:slide-in-from-top-[48%]",
 				className,
 			)}
 			{...props}
