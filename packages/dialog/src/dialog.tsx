bddb0336fb279fb04e8ea12d94f2e2d631bcbfb9
Aaron Shekey <aaron@aaronshekey.com>
2024-05-22 10:49:49 -0500

    Dialog animation refinement (#236)
    * Only transition the transform property
    * Use rems for the inset

--- packages/dialog/src/dialog.tsx
+++ packages/dialog/src/dialog.tsx
@@ -37,7 +37,7 @@ const DialogContent = forwardRef<
 		<DialogPrimitive.Content
 			ref={ref}
 			className={cx(
-				"fixed left-1/2 top-1/2 z-50 flex max-h-[calc(100dvh_-_32px)] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 flex-col rounded-xl border border-dialog bg-dialog shadow-lg duration-200",
+				"fixed left-1/2 top-1/2 z-50 flex max-h-[calc(100dvh_-_2rem)] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 flex-col rounded-xl border border-dialog bg-dialog shadow-lg transition-transform duration-200",
 				"data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-closed:slide-out-to-left-1/2 data-state-closed:slide-out-to-top-[48%] data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95 data-state-open:slide-in-from-left-1/2 data-state-open:slide-in-from-top-[48%]",
 				className,
 			)}
