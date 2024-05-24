0806e469b93a5803eadd5c5e8feae091b2c9647d
Aaron Shekey <aaron@aaronshekey.com>
2024-05-24 13:34:09 -0500

    Ope, protecc the selected state

--- packages/calendar/src/calendar.tsx
+++ packages/calendar/src/calendar.tsx
@@ -31,7 +31,7 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
 						? "first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
 						: "",
 				),
-				day: "day sm:h-7 sm:w-7 h-7 w-7 rounded-md hover:bg-filled-accent/15",
+				day: "day sm:h-7 sm:w-7 h-7 w-7 rounded-md [&:not([aria-selected])]:hover:bg-filled-accent/15",
 				day_range_start: "day-range-start [&:not(.day-range-end)]:rounded-r-none",
 				day_range_end: "day-range-end [&:not(.day-range-start)]:rounded-l-none",
 				day_selected: "bg-filled-accent text-on-filled hover:bg-filled-accent",
