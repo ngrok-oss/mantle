243e39743ac212e8ccefed5119b7c8cf4eb12c0f
Cody A Price <cody@ngrok.com>
2024-05-24 16:19:46 -0500

    runs "bun fmt"

--- app/routes/base.tailwind-variants.tsx
+++ app/routes/base.tailwind-variants.tsx
@@ -29,25 +29,25 @@ export default function Page() {
 					firefox: <Icon className="hidden firefox:block" svg={<CheckCircle />} />
 					<Icon className="block firefox:hidden" svg={<XCircle />} />
 				</li>
-				<li className="pointer-coarse:border-green-600 pointer-coarse:bg-green-600/10 flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4">
-					pointer-coarse: <Icon className="pointer-coarse:block hidden" svg={<CheckCircle />} />
-					<Icon className="pointer-coarse:hidden block" svg={<XCircle />} />
+				<li className="flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4 pointer-coarse:border-green-600 pointer-coarse:bg-green-600/10">
+					pointer-coarse: <Icon className="hidden pointer-coarse:block" svg={<CheckCircle />} />
+					<Icon className="block pointer-coarse:hidden" svg={<XCircle />} />
 				</li>
-				<li className="pointer-fine:border-green-600 pointer-fine:bg-green-600/10 flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4">
-					pointer-fine: <Icon className="pointer-fine:block hidden" svg={<CheckCircle />} />
-					<Icon className="pointer-fine:hidden block" svg={<XCircle />} />
+				<li className="flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4 pointer-fine:border-green-600 pointer-fine:bg-green-600/10">
+					pointer-fine: <Icon className="hidden pointer-fine:block" svg={<CheckCircle />} />
+					<Icon className="block pointer-fine:hidden" svg={<XCircle />} />
 				</li>
-				<li className="pointer-none:border-green-600 pointer-none:bg-green-600/10 flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4">
-					pointer-none: <Icon className="pointer-none:block hidden" svg={<CheckCircle />} />
-					<Icon className="pointer-none:hidden block" svg={<XCircle />} />
+				<li className="flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4 pointer-none:border-green-600 pointer-none:bg-green-600/10">
+					pointer-none: <Icon className="hidden pointer-none:block" svg={<CheckCircle />} />
+					<Icon className="block pointer-none:hidden" svg={<XCircle />} />
 				</li>
-				<li className="hover-hover:border-green-600 hover-hover:bg-green-600/10 flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4">
-					hover-hover: <Icon className="hover-hover:block hidden" svg={<CheckCircle />} />
-					<Icon className="hover-hover:hidden block" svg={<XCircle />} />
+				<li className="flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4 hover-hover:border-green-600 hover-hover:bg-green-600/10">
+					hover-hover: <Icon className="hidden hover-hover:block" svg={<CheckCircle />} />
+					<Icon className="block hover-hover:hidden" svg={<XCircle />} />
 				</li>
-				<li className="hover-none:border-green-600 hover-none:bg-green-600/10 flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4">
-					hover-none: <Icon className="hover-none:block hidden" svg={<CheckCircle />} />
-					<Icon className="hover-none:hidden block" svg={<XCircle />} />
+				<li className="flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4 hover-none:border-green-600 hover-none:bg-green-600/10">
+					hover-none: <Icon className="hidden hover-none:block" svg={<CheckCircle />} />
+					<Icon className="block hover-none:hidden" svg={<XCircle />} />
 				</li>
 				<li className="flex size-28 flex-col items-center justify-center gap-1 rounded-lg border border-red-600 bg-red-600/10 p-4 dark-high-contrast:border-green-600 dark-high-contrast:bg-green-600/10">
 					dark-high-contrast: <Icon className="hidden dark-high-contrast:block" svg={<CheckCircle />} />
