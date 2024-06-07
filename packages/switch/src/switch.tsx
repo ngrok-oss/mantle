fa8a5005a7a6eeb1989c003041da0f4a573a184b
Cody A Price <cody@ngrok.com>
2024-06-07 03:16:38 -0500

    more style tweaks... slightly better light mode?

--- packages/switch/src/switch.tsx
+++ packages/switch/src/switch.tsx
@@ -8,10 +8,10 @@ const Switch = forwardRef<ElementRef<typeof SwitchPrimitiveRoot>, ComponentProps
 	({ className, ...props }, ref) => (
 		<SwitchPrimitiveRoot
 			className={cx(
-				"peer inline-flex h-6 w-10 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm outline-none transition-colors sm:h-5 sm:w-9",
+				"peer inline-flex h-6 w-10 shrink-0 cursor-pointer items-center rounded-full border border-form shadow-sm outline-none transition-colors sm:h-5 sm:w-9",
 				"disabled:cursor-default disabled:opacity-50",
-				"focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-focus-accent",
-				"data-state-checked:bg-blue-500 data-state-unchecked:bg-form",
+				"focus-visible:border-accent-600 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-focus-accent",
+				"data-state-checked:bg-blue-500 data-state-unchecked:bg-form-active",
 				className,
 			)}
 			{...props}
