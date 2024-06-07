235589b2e88f95b65cf73e98e24bc21c41839ec2
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2024-06-07 11:10:21 -0500

    adds switch component (#254)
    * adds switch component
    
    * minor style tweaks
    
    * more style tweaks... slightly better light mode?
    
    * another one: stylin 💅
    
    * more bun lockfile bs
    
    * No shadow or border
    
    * What if we don't transition the color?
    
    * move parse-booleanish -> types/src/booleanish, widen args and add tests for null/undefined
    
    * add readOnly prop (does not really work tho)
    
    * (slightly) improve switch examples
    
    ---------
    
    Co-authored-by: Aaron Shekey <aaron@aaronshekey.com>

--- packages/types/src/booleanish.tsx
+++ packages/types/src/booleanish.tsx
@@ -4,7 +4,7 @@ export type Booleanish = boolean | "true" | "false";
  * Parse/coerce a booleanish value (boolean | "true" | "false") into a boolean.
  * @default false if the value is not a boolean or "true"
  */
-export function parseBooleanish(value: Booleanish | (string & {})): boolean {
+export function parseBooleanish(value: Booleanish | (string & {}) | undefined | null): boolean {
 	if (typeof value === "boolean") {
 		return value;
 	} else {
