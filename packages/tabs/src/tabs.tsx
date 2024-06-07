db5b13a11c080e803cfa922d3b682efb80ae4c45
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2024-06-07 13:53:54 -0500

    polish Tabs (#256)
    * add missing border, fix how the active chrome renders for vertical tabs
    
    * fmt
    
    * overlay active border over tabslist gray border
    
    * make tabslist gray border grow to container size

--- packages/tabs/src/tabs.tsx
+++ packages/tabs/src/tabs.tsx
@@ -4,6 +4,7 @@ import {
 	Root as TabsPrimitiveRoot,
 	Trigger as TabsPrimitiveTrigger,
 } from "@radix-ui/react-tabs";
+import clsx from "clsx";
 import { createContext, forwardRef, useContext } from "react";
 import type { ComponentPropsWithoutRef, ElementRef, HTMLAttributes } from "react";
 import { cx } from "../../cx";
@@ -36,8 +37,10 @@ const TabsList = forwardRef<ElementRef<typeof TabsPrimitiveList>, ComponentProps
 			<TabsPrimitiveList
 				aria-orientation={ctx.orientation}
 				className={cx(
-					"flex",
-					ctx.orientation === "horizontal" ? "flex-row items-center gap-6" : "flex-col items-end gap-[0.875rem]",
+					"flex border-gray-200",
+					ctx.orientation === "horizontal"
+						? "flex-row items-center gap-6 border-b"
+						: "flex-col items-end gap-[0.875rem] self-stretch border-r",
 					className,
 				)}
 				ref={ref}
@@ -51,29 +54,38 @@ TabsList.displayName = "TabsList";
 type TabsTriggerProps = ComponentPropsWithoutRef<typeof TabsPrimitiveTrigger>;
 
 const TabsTrigger = forwardRef<ElementRef<typeof TabsPrimitiveTrigger>, TabsTriggerProps>(
-	({ children, className, ...props }, ref) => (
-		<TabsPrimitiveTrigger
-			className={cx(
-				"group/tab-trigger relative flex cursor-pointer items-center gap-1 whitespace-nowrap py-3 text-sm font-medium text-gray-600",
-				"rounded-tl-md rounded-tr-md",
-				"outline-none ring-focus-accent",
-				"disabled:cursor-default disabled:opacity-50",
-				"focus-visible:ring-4",
-				"[&>svg]:size-6 [&>svg]:shrink-0 [&>svg]:sm:size-5",
-				"enabled:hover:text-gray-900 enabled:hover:data-state-active:text-blue-600",
-				"data-state-active:text-blue-600",
-				className,
-			)}
-			ref={ref}
-			{...props}
-		>
-			<span
-				aria-hidden
-				className="absolute bottom-0 left-0 right-0 h-[0.1875rem] group-data-state-active/tab-trigger:bg-blue-600"
-			/>
-			{children}
-		</TabsPrimitiveTrigger>
-	),
+	({ children, className, ...props }, ref) => {
+		const ctx = useContext(TabsStateContext);
+
+		return (
+			<TabsPrimitiveTrigger
+				className={cx(
+					"group/tab-trigger relative flex cursor-pointer items-center gap-1 whitespace-nowrap py-3 text-sm font-medium text-gray-600",
+					ctx.orientation === "horizontal" && "rounded-tl-md rounded-tr-md",
+					ctx.orientation === "vertical" && "rounded-bl-md rounded-tl-md pr-3",
+					"outline-none ring-focus-accent",
+					"disabled:cursor-default disabled:opacity-50",
+					"focus-visible:ring-4",
+					"[&>svg]:size-6 [&>svg]:shrink-0 [&>svg]:sm:size-5",
+					"enabled:hover:text-gray-900 enabled:hover:data-state-active:text-blue-600",
+					"data-state-active:text-blue-600",
+					className,
+				)}
+				ref={ref}
+				{...props}
+			>
+				<span
+					aria-hidden
+					className={clsx(
+						"absolute z-1 group-data-state-active/tab-trigger:bg-blue-600",
+						ctx.orientation === "horizontal" && "-bottom-px left-0 right-0 h-[0.1875rem]",
+						ctx.orientation === "vertical" && "-right-px bottom-0 top-0 w-[0.1875rem]",
+					)}
+				/>
+				{children}
+			</TabsPrimitiveTrigger>
+		);
+	},
 );
 TabsTrigger.displayName = "TabsTrigger";
 
