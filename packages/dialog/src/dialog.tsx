113afef50c221e8a90b9b1f851edf7c7353371b1
Cody A Price <cody@ngrok.com>
2024-06-27 15:00:55 -0500

    fix DialogContent onClickOutside closing dialog behavior

--- packages/dialog/src/dialog.tsx
+++ packages/dialog/src/dialog.tsx
@@ -32,19 +32,20 @@ const DialogContent = forwardRef<ElementRef<"div">, ComponentPropsWithoutRef<typ
 	({ className, children, ...props }, ref) => (
 		<DialogPortal>
 			<DialogOverlay />
-			<DialogPrimitive.Content className="fixed inset-4 z-50 flex items-center justify-center" {...props}>
-				<div
+			<div className="fixed inset-4 z-50 flex items-center justify-center">
+				<DialogPrimitive.Content
 					className={cx(
 						"flex max-h-full w-full max-w-lg flex-1 flex-col",
 						"rounded-xl border border-dialog bg-dialog shadow-lg transition-transform duration-200",
-						"data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-closed:slide-out-to-left-1/2 data-state-closed:slide-out-to-top-[48%] data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95 data-state-open:slide-in-from-left-1/2 data-state-open:slide-in-from-top-[48%]",
+						"data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95",
 						className,
 					)}
 					ref={ref}
+					{...props}
 				>
 					{children}
-				</div>
-			</DialogPrimitive.Content>
+				</DialogPrimitive.Content>
+			</div>
 		</DialogPortal>
 	),
 );
