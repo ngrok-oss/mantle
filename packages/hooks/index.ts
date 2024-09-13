d767248d1332870e3a64f72581743baf551f2096
Cody A Price <cody@ngrok.com>
2024-09-13 14:40:23 -0500

    remove dependency on @uidotdev/usehooks

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,3 +1,4 @@
+export { useCopyToClipboard } from "./src/use-copy-to-clipboard";
 export { useIsomorphicLayoutEffect } from "./src/use-isomorphic-layout-effect";
 export { useMatchesMediaQuery } from "./src/use-matches-media-query";
 export { usePrefersReducedMotion } from "./src/use-prefers-reduced-motion";
