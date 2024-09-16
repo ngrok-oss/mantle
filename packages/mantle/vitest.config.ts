01838a76c55c2dfc368ea2f35be2457a16f44477
Melanie Seltzer <m.seltzer@ngrok.com>
2024-09-16 09:15:28 -0700

    clean up vitest config

--- packages/mantle/vitest.config.ts
+++ packages/mantle/vitest.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "./vite.config";
+import { defineConfig } from "vitest/config";
 
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			globals: true,
-			environment: "jsdom",
-			setupFiles: "./vitest.setup.ts",
-			// you might want to disable it, if you don't have tests that rely on CSS
-			// since parsing CSS is slow
-			css: true,
-		},
-	}),
-);
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./vitest.setup.ts",
+		css: true,
+	},
+});
