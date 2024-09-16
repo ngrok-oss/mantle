a960aa07df4b2106438219e273f5fca3367cb723
Melanie Seltzer <m.seltzer@ngrok.com>
2024-09-16 14:02:54 -0700

    fix tailwind config

--- apps/docs/remix.config.js
+++ apps/docs/remix.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
 	ignoredRouteFiles: ["**/.*"],
-	watchPaths: ["./tailwind.config.ts", "../../packages/mantle/**/*"],
+	watchPaths: ["./tailwind.config.ts", "../../packages/mantle/dist/**/*"],
 };
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,10 +1,7 @@
-import { createRequire } from "node:module";
-import { mantlePreset, resolveMantleContentGlob } from "@ngrok/mantle/tailwind-preset";
+import { mantlePreset } from "@ngrok/mantle/tailwind-preset";
 import type { Config } from "tailwindcss";
-
-const require = createRequire(import.meta.url);
 
 export default {
 	presets: [mantlePreset],
-	content: [resolveMantleContentGlob(require), "./app/**/*.tsx"],
+	content: ["./app/**/*.tsx", "../../packages/mantle/dist/**/*.js"],
 } satisfies Config;
