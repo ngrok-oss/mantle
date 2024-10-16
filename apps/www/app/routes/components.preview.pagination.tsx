13db41da4b9a4a48a4265d00fdb17ca1968f5a9e
Cody A Price <cody@ngrok.com>
2024-10-15 19:54:44 -0500

    add new pagination route + CursorPageSizeValue component

---
+++ apps/www/app/routes/components.preview.pagination.tsx
@@ -0,0 +1,264 @@
+import { CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton, fmtCode } from "@ngrok/mantle/code-block";
+import { CursorButtons, CursorPageSizeSelect, CursorPagination } from "@ngrok/mantle/pagination";
+import type { HeadersFunction, MetaFunction } from "@remix-run/node";
+import { PreviewBadge } from "~/components/badges";
+import { Example } from "~/components/example";
+import { CursorPageSizeValue } from "node_modules/@ngrok/mantle/src/components/pagination/cursor-pagination";
+
+// import {
+// 	BooleanPropType,
+// 	PropDefaultValueCell,
+// 	PropDescriptionCell,
+// 	PropNameCell,
+// 	PropRow,
+// 	PropsTable,
+// 	PropTypeCell,
+// 	ReactNodePropType,
+// 	StringPropType,
+// } from "~/components/props-table";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "@ngrok/mantle — Pagination" },
+		{ name: "description", content: "mantle is ngrok's UI library and design system" },
+	];
+};
+
+export const headers: HeadersFunction = () => {
+	return {
+		"Cache-Control": "max-age=300, stale-while-revalidate=604800",
+	};
+};
+
+export default function Page() {
+	return (
+		<div className="space-y-16">
+			<section className="space-y-4">
+				<div className="flex items-center gap-3">
+					<h1 id="calendar" className="text-5xl font-medium">
+						Pagination
+					</h1>
+					<PreviewBadge />
+				</div>
+				<p className="font-body text-body text-xl">A date field component that allows users to enter and edit date.</p>
+				<div>
+					<Example className="flex flex-col gap-6">
+						<CursorPagination defaultPageSize={100}>
+							<CursorPageSizeSelect pageSizes={[]} />
+							<CursorButtons hasNextPage hasPreviousPage />
+						</CursorPagination>
+
+						<CursorPagination defaultPageSize={100}>
+							<CursorPageSizeValue />
+							<CursorButtons hasNextPage hasPreviousPage />
+						</CursorPagination>
+
+						<CursorPagination defaultPageSize={100}>
+							<CursorButtons hasNextPage hasPreviousPage={false} />
+						</CursorPagination>
+					</Example>
+					<CodeBlock className="rounded-b-lg rounded-t-none">
+						<CodeBlockBody>
+							<CodeBlockCopyButton />
+							<CodeBlockCode
+								language="tsx"
+								value={fmtCode`
+									import { CursorButtons, CursorPageSizeSelect, CursorPagination } from "@ngrok/mantle/pagination";
+
+									<CursorPagination defaultPageSize={100}>
+										<CursorPageSizeSelect />
+										<CursorButtons hasNextPage hasPreviousPage />
+									</CursorPagination>
+
+									<CursorPagination defaultPageSize={100}>
+										<CursorPageSizeValue />
+										<CursorButtons hasNextPage hasPreviousPage />
+									</CursorPagination>
+
+									<CursorPagination defaultPageSize={20}>
+										<CursorButtons hasNextPage hasPreviousPage={false} />
+									</CursorPagination>
+								`}
+							/>
+						</CodeBlockBody>
+					</CodeBlock>
+				</div>
+			</section>
+
+			<section className="space-y-4">
+				<h2 id="api" className="text-3xl font-medium">
+					API Reference
+				</h2>
+				{/* <p className="font-body text-body text-xl">
+					The <InlineCode>Calendar</InlineCode> is built on top of{" "}
+					<Anchor href="https://react-day-picker.js.org/">React DayPicker</Anchor>.
+				</p> */}
+				{/* <PropsTable>
+					<PropRow>
+						<PropNameCell name="appearance" optional />
+						<PropTypeCell>
+							<ul>
+								<li>
+									<StringPropType value="ghost" />
+								</li>
+								<li>
+									<StringPropType value="filled" />
+								</li>
+								<li>
+									<StringPropType value="outlined" />
+								</li>
+								<li>
+									<StringPropType value="link" />
+								</li>
+							</ul>
+						</PropTypeCell>
+						<PropDefaultValueCell>
+							<StringPropType value="outlined" />
+						</PropDefaultValueCell>
+						<PropDescriptionCell>
+							<p>
+								Defines the visual style of the <InlineCode>Button</InlineCode>.
+							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="asChild" optional />
+						<PropTypeCell>
+							<BooleanPropType />
+						</PropTypeCell>
+						<PropDefaultValueCell>
+							<BooleanPropType value={false} />
+						</PropDefaultValueCell>
+						<PropDescriptionCell>
+							<p>
+								Use the <InlineCode>asChild</InlineCode> prop to compose the <InlineCode>Button</InlineCode> styling and
+								functionality onto alternative element types or your own React components.
+							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="icon" optional />
+						<PropTypeCell>
+							<ReactNodePropType />
+						</PropTypeCell>
+						<PropDefaultValueCell />
+						<PropDescriptionCell>
+							An icon to render inside the button. If the <InlineCode>state</InlineCode> is{" "}
+							<InlineCode>pending</InlineCode>, then the icon will automatically be replaced with a spinner.
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="iconPlacement" optional />
+						<PropTypeCell>
+							<ul>
+								<li>
+									<StringPropType value="start" />
+								</li>
+								<li>
+									<StringPropType value="end" />
+								</li>
+							</ul>
+						</PropTypeCell>
+						<PropDefaultValueCell>
+							<StringPropType value="start" />
+						</PropDefaultValueCell>
+						<PropDescriptionCell>
+							<p>
+								The side that the icon will render on, if one is present. If <InlineCode>state="pending"</InlineCode>,
+								then the loading icon will also render on this side.
+							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="isLoading" optional />
+						<PropTypeCell>
+							<BooleanPropType />
+						</PropTypeCell>
+						<PropDefaultValueCell>
+							<BooleanPropType value={false} />
+						</PropDefaultValueCell>
+						<PropDescriptionCell>
+							<p>
+								Determines whether or not the button is in a loading state, default <InlineCode>false</InlineCode>.
+								Setting <InlineCode>isLoading</InlineCode> will replace any <InlineCode>icon</InlineCode> with a
+								spinner, or add one if an icon wasn't given. It will also disable user interaction with the button and
+								set <InlineCode>aria-disabled</InlineCode>.
+							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="priority" optional />
+						<PropTypeCell>
+							<ul>
+								<li>
+									<StringPropType value="default" />
+								</li>
+								<li>
+									<StringPropType value="danger" />
+								</li>
+								<li>
+									<StringPropType value="neutral" />
+								</li>
+							</ul>
+						</PropTypeCell>
+						<PropDefaultValueCell>
+							<StringPropType value="default" />
+						</PropDefaultValueCell>
+						<PropDescriptionCell>
+							<p>
+								Indicates the importance or impact level of the button, affecting its color and styling to communicate
+								its purpose to the user.
+							</p>
+						</PropDescriptionCell>
+					</PropRow>
+					<PropRow>
+						<PropNameCell name="type" />
+						<PropTypeCell>
+							<ul>
+								<li>
+									<StringPropType value="button" />
+								</li>
+								<li>
+									<StringPropType value="reset" />
+								</li>
+								<li>
+									<StringPropType value="submit" />
+								</li>
+							</ul>
+						</PropTypeCell>
+						<PropDefaultValueCell />
+						<PropDescriptionCell>
+							<p>
+								The default behavior of the <InlineCode>Button</InlineCode>. Unlike the native{" "}
+								<InlineCode>button</InlineCode> element, unless you use the <InlineCode>asChild</InlineCode> prop,{" "}
+								<span className="font-semibold">this prop is required and has no default value</span>. See{" "}
+								<Anchor href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#type">
+									the MDN docs
+								</Anchor>{" "}
+								for more information.
+							</p>
+							<ul className="list-disc pl-5">
+								<li>
+									<p>
+										<InlineCode>"button"</InlineCode>: The button has no default behavior, and does nothing when pressed
+										by default.
+									</p>
+								</li>
+								<li>
+									<p>
+										<InlineCode>"reset"</InlineCode>: The button resets all the controls to their initial values.
+									</p>
+								</li>
+								<li>
+									<p>
+										<InlineCode>"submit"</InlineCode>: The button submits the form data to the server.
+									</p>
+								</li>
+							</ul>
+						</PropDescriptionCell>
+					</PropRow>
+				</PropsTable> */}
+			</section>
+		</div>
+	);
+}
