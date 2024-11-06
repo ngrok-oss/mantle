9f9c8336c25135f9d41d855d942567890802b50f
Cody A Price <cody@ngrok.com>
2024-11-06 10:33:04 -0600

    icon: support forwarded refs (so asChild on parents works)

--- packages/mantle/src/components/icon/icon.tsx
+++ packages/mantle/src/components/icon/icon.tsx
@@ -1,9 +1,9 @@
-import type { ReactNode } from "react";
-import type { WithStyleProps } from "../../types/with-style-props.js";
+import { forwardRef, type ElementRef, type ReactNode } from "react";
 import { cx } from "../../utils/cx/cx.js";
 import { IconBase } from "./_icon-base.js";
+import type { SvgAttributes } from "./types.js";
 
-type IconProps = WithStyleProps & {
+type IconProps = Omit<SvgAttributes, "children"> & {
 	/**
 	 * A single SVG icon element.
 	 */
@@ -16,9 +16,16 @@ type IconProps = WithStyleProps & {
  * 2. svg className
  * 3. Icon className
  */
-const Icon = ({ className, style, svg }: IconProps) => (
-	<IconBase className={cx("size-6 sm:size-5", className)} style={style} svg={svg} />
-);
+const Icon = forwardRef<ElementRef<"svg">, IconProps>(({ className, style, svg, ...props }, ref) => (
+	<IconBase ref={ref} className={cx("size-6 sm:size-5", className)} style={style} svg={svg} {...props} />
+));
 
-export { Icon };
-export type { IconProps };
+export {
+	//,
+	Icon,
+};
+
+export type {
+	//,
+	IconProps,
+};
