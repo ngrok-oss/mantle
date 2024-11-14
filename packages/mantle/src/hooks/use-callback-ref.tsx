eed4b9ceda2ce83ade1d7309c7d3e308e92b46ed
Cody A Price <cody@ngrok.com>
2024-11-14 09:36:32 -0600

    hooks: add useCallbackRef and useDebouncedCallback

---
+++ packages/mantle/src/hooks/use-callback-ref.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useMemo, useRef } from "react";
+
+/**
+ * Returns a memoized callback that will always refer to the latest callback
+ * passed to the hook.
+ *
+ * This is useful when you want to pass a callback which may or may not be
+ * memoized (have a stable identity) to a child component that will be updated
+ * without causing the child component to re-render.
+ */
+function useCallbackRef<T extends (...args: any[]) => any>(callback: T | undefined): T {
+	const callbackRef = useRef(callback);
+
+	useEffect(() => {
+		callbackRef.current = callback;
+	});
+
+	return useMemo(() => ((...args) => callbackRef.current?.(...args)) as T, []);
+}
+
+export {
+	//,
+	useCallbackRef,
+};
