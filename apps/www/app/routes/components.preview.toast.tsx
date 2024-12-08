b80bc367b6b258e3b823e553332508a978bdfad5
Cody A Price <cody@ngrok.com>
2024-12-07 19:54:56 -0600

    add toast component

---
+++ apps/www/app/routes/components.preview.toast.tsx
@@ -0,0 +1,251 @@
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogIcon,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@ngrok/mantle/alert-dialog";
+import { Button, IconButton } from "@ngrok/mantle/button";
+import { CodeBlock, CodeBlockBody, CodeBlockCode, CodeBlockCopyButton, fmtCode } from "@ngrok/mantle/code-block";
+import {
+	Dialog,
+	DialogBody,
+	DialogClose,
+	DialogCloseIconButton,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@ngrok/mantle/dialog";
+import {
+	Sheet,
+	SheetBody,
+	SheetClose,
+	SheetContent,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@ngrok/mantle/sheet";
+import { makeToast, Toast, ToastAction, ToastIcon, ToastMessage, type Priority } from "@ngrok/mantle/toast";
+import { X } from "@phosphor-icons/react/X";
+import type { HeadersFunction, MetaFunction } from "@remix-run/node";
+import { PreviewBadge } from "~/components/badges";
+import { Example } from "~/components/example";
+import { useState } from "react";
+import invariant from "tiny-invariant";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "@ngrok/mantle — Toast" },
+		{ name: "description", content: "mantle is ngrok's UI library and design system" },
+	];
+};
+
+export const headers: HeadersFunction = () => {
+	return {
+		"Cache-Control": "max-age=300, stale-while-revalidate=604800",
+	};
+};
+
+const priorities = ["info", "success", "warning", "danger"] as const satisfies Priority[];
+
+function getPriorityByIndex(index: number): Priority {
+	const priority = priorities[index % priorities.length];
+	invariant(priority, `Invalid priority index: ${index}`);
+	return priority;
+}
+
+export default function Page() {
+	const [priorityIndex, setPriorityIndex] = useState(0);
+	const priority = getPriorityByIndex(priorityIndex);
+
+	return (
+		<div className="space-y-4">
+			<div className="flex items-center gap-3">
+				<h1 className="text-5xl font-medium">Toast</h1>
+				<PreviewBadge />
+			</div>
+			<p className="font-body text-body text-xl">
+				Pariatur eiusmod consectetur adipisicing sint sunt duis labore ad officia aliquip minim exercitation.
+			</p>
+			<div>
+				<Example className="flex-col gap-4">
+					<Button
+						type="button"
+						appearance="filled"
+						onClick={() => {
+							makeToast(
+								<Toast priority={priority}>
+									<ToastIcon />
+									<ToastMessage>
+										Laborum ea anim adipisicing in Lorem incididunt mollit ipsum reprehenderit.
+									</ToastMessage>
+									<ToastAction asChild>
+										<IconButton type="button" appearance="ghost" size="xs" icon={<X />} label="Dismiss toast" />
+									</ToastAction>
+								</Toast>,
+							);
+
+							setPriorityIndex((prev) => prev + 1);
+						}}
+					>
+						Show Toast
+					</Button>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button type="button" appearance="filled">
+								Open Sheet
+							</Button>
+						</SheetTrigger>
+						<SheetContent>
+							<SheetHeader>
+								<SheetTitle>Are you absolutely sure?</SheetTitle>
+							</SheetHeader>
+							<SheetBody className="space-y-4">
+								<Button
+									type="button"
+									onClick={() =>
+										makeToast(
+											<Toast priority="warning">
+												<ToastIcon />
+												<ToastMessage>
+													Laborum ea anim adipisicing in Lorem incididunt mollit ipsum reprehenderit.
+												</ToastMessage>
+												<ToastAction asChild>
+													<IconButton type="button" appearance="ghost" size="xs" icon={<X />} label="Dismiss toast" />
+												</ToastAction>
+											</Toast>,
+										)
+									}
+								>
+									Show Toast
+								</Button>
+							</SheetBody>
+							<SheetFooter>
+								<SheetClose asChild>
+									<Button type="button">Close</Button>
+								</SheetClose>
+							</SheetFooter>
+						</SheetContent>
+					</Sheet>
+					<Dialog>
+						<DialogTrigger asChild>
+							<Button type="button" appearance="filled">
+								Open dialog
+							</Button>
+						</DialogTrigger>
+						<DialogContent>
+							<DialogHeader>
+								<DialogTitle>Are you absolutely sure? aslkdfjas dlfksdoijfoasdjf ioadsjfi</DialogTitle>
+								<DialogCloseIconButton />
+							</DialogHeader>
+							<DialogBody>
+								<Button
+									type="button"
+									onClick={() =>
+										makeToast(
+											<Toast priority="danger">
+												<ToastIcon />
+												<ToastMessage>
+													Laborum ea anim adipisicing in Lorem incididunt mollit ipsum reprehenderit.
+												</ToastMessage>
+												<ToastAction asChild>
+													<IconButton type="button" appearance="ghost" size="xs" icon={<X />} label="Dismiss toast" />
+												</ToastAction>
+											</Toast>,
+										)
+									}
+								>
+									Show Toast
+								</Button>
+							</DialogBody>
+							<DialogFooter>
+								<DialogClose asChild>
+									<Button type="button" priority="neutral" appearance="outlined">
+										Cancel
+									</Button>
+								</DialogClose>
+							</DialogFooter>
+						</DialogContent>
+					</Dialog>
+					<AlertDialog priority="info">
+						<AlertDialogTrigger asChild>
+							<Button type="button" appearance="filled">
+								Show Info Alert Dialog
+							</Button>
+						</AlertDialogTrigger>
+						<AlertDialogContent>
+							<AlertDialogIcon />
+							<AlertDialogBody>
+								<AlertDialogHeader>
+									<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+								</AlertDialogHeader>
+								<Button
+									type="button"
+									onClick={() =>
+										makeToast(
+											<Toast priority="success">
+												<ToastIcon />
+												<ToastMessage>
+													Laborum ea anim adipisicing in Lorem incididunt mollit ipsum reprehenderit.
+												</ToastMessage>
+												<ToastAction asChild>
+													<IconButton type="button" appearance="ghost" size="xs" icon={<X />} label="Dismiss toast" />
+												</ToastAction>
+											</Toast>,
+										)
+									}
+								>
+									Show Toast
+								</Button>
+								<AlertDialogFooter>
+									<AlertDialogCancel type="button">Cancel</AlertDialogCancel>
+								</AlertDialogFooter>
+							</AlertDialogBody>
+						</AlertDialogContent>
+					</AlertDialog>
+				</Example>
+				<CodeBlock className="rounded-b-lg rounded-t-none">
+					<CodeBlockBody>
+						<CodeBlockCopyButton />
+						<CodeBlockCode
+							language="tsx"
+							value={fmtCode`
+								import { Button } from "@ngrok/mantle/button";
+								import { makeToast, Toast, ToastAction, ToastIcon, ToastMessage } from "@ngrok/mantle/toast";
+
+								// Only one of these should be added to the root of your app
+								<Toaster />
+
+								<Button
+									type="button"
+									onClick={() =>
+										makeToast(
+											<Toast priority="success">
+												<ToastIcon />
+												<ToastMessage>
+													Laborum ea anim adipisicing in Lorem incididunt mollit ipsum reprehenderit.
+												</ToastMessage>
+												<ToastAction asChild>
+													<IconButton type="button" appearance="ghost" size="xs" icon={<X />} label="Dismiss toast" />
+												</ToastAction>
+											</Toast>,
+										)
+									}
+								>
+									Show Toast
+								</Button>
+							`}
+						/>
+					</CodeBlockBody>
+				</CodeBlock>
+			</div>
+		</div>
+	);
+}
---
+++ packages/mantle/src/components/toast/toast.tsx
@@ -0,0 +1,220 @@
+"use client";
+
+import { CheckCircle } from "@phosphor-icons/react/CheckCircle";
+import { Info } from "@phosphor-icons/react/Info";
+import { Warning } from "@phosphor-icons/react/Warning";
+import { WarningDiamond } from "@phosphor-icons/react/WarningDiamond";
+import { Slot } from "@radix-ui/react-slot";
+import { createContext, forwardRef, useContext, type ComponentProps, type ElementRef, type ReactNode } from "react";
+import * as ToastPrimitive from "sonner";
+import type { WithAsChild } from "../../types/as-child.js";
+import { cx } from "../../utils/cx/cx.js";
+import { Icon } from "../icon/icon.js";
+import type { SvgOnlyProps } from "../icon/svg-only.js";
+
+type ToasterProps = React.ComponentProps<typeof ToastPrimitive.Toaster>;
+
+const Toaster = (props: ToasterProps) => {
+	return (
+		<ToastPrimitive.Toaster
+			position="top-center"
+			className="toaster mantle-prompt pointer-events-auto font-sans"
+			gap={12}
+			toastOptions={{
+				unstyled: true,
+			}}
+			{...props}
+		/>
+	);
+};
+
+const ToastIdContext = createContext<string | number>("");
+
+function makeToast(children: ReactNode) {
+	return ToastPrimitive.toast.custom(
+		(toastId) => <ToastIdContext.Provider value={toastId}>{children}</ToastIdContext.Provider>,
+		{
+			//
+		},
+	);
+}
+
+type ToastActionProps = ComponentProps<"button"> & WithAsChild;
+
+const priorities = ["info", "success", "warning", "danger"] as const;
+type Priority = (typeof priorities)[number];
+
+type ToastState = {
+	priority: Priority;
+};
+
+const ToastStateContext = createContext<ToastState>({
+	priority: "info",
+});
+
+type ToastProps = ComponentProps<"div"> &
+	WithAsChild & {
+		priority: Priority;
+	};
+
+const Toast = forwardRef<ElementRef<"div">, ToastProps>(({ asChild, children, className, priority, ...props }, ref) => {
+	const Component = asChild ? Slot : "div";
+
+	return (
+		<ToastStateContext.Provider value={{ priority }}>
+			<Component
+				data-mantle-notifier
+				className={cx(
+					"relative flex items-start gap-2",
+					"py-3 pl-[0.9375rem] pr-4",
+					"bg-popover border-popover rounded border shadow-lg",
+					/**
+					 * Do not apply overflow-hidden because we want the priority bar accent
+					 * to overlap the toast border, else the border flows over the
+					 * priority bar.
+					 */
+					className,
+				)}
+				ref={ref}
+				{...props}
+			>
+				<PriorityBarAccent priority={priority} />
+				{children}
+			</Component>
+		</ToastStateContext.Provider>
+	);
+});
+
+const priorityBackgroundColor = {
+	info: "bg-accent-600",
+	warning: "bg-warning-600",
+	success: "bg-success-600",
+	danger: "bg-danger-600",
+} as const satisfies Record<Priority, string>;
+
+type PriorityBarAccentProps = Omit<ComponentProps<"div">, "children"> & { priority: Priority };
+
+function PriorityBarAccent({ className, priority, ...props }: PriorityBarAccentProps) {
+	return (
+		<div
+			className={cx(
+				"z-1 absolute -inset-px right-auto w-1.5 rounded-bl rounded-tl",
+				priorityBackgroundColor[priority],
+				className,
+			)}
+		/>
+	);
+}
+
+type ToastIconProps = Partial<SvgOnlyProps>;
+
+const defaultPriorityIcon = {
+	danger: <Warning weight="fill" />,
+	warning: <WarningDiamond weight="fill" />,
+	success: <CheckCircle weight="fill" />,
+	info: <Info weight="fill" />,
+} as const satisfies Record<Priority, ReactNode>;
+
+const ToastIcon = forwardRef<ElementRef<"svg">, ToastIconProps>(({ className, svg, ...props }, ref) => {
+	const ctx = useContext(ToastStateContext);
+
+	switch (ctx.priority) {
+		case "danger":
+			return (
+				<Icon
+					className={cx("text-danger-600", className)}
+					ref={ref}
+					svg={svg ?? <Warning weight="fill" />}
+					{...props}
+				/>
+			);
+		case "warning":
+			return (
+				<Icon
+					className={cx("text-warning-600", className)}
+					ref={ref}
+					svg={svg ?? <WarningDiamond weight="fill" />}
+					{...props}
+				/>
+			);
+		case "success":
+			return (
+				<Icon
+					className={cx("text-success-600", className)}
+					ref={ref}
+					svg={svg ?? <CheckCircle weight="fill" />}
+					{...props}
+				/>
+			);
+		case "info":
+			return <Icon className={cx("text-accent-600", className)} ref={ref} svg={<Info weight="fill" />} {...props} />;
+		default:
+			throw new Error(`Unreachable Case: ${ctx.priority}`);
+	}
+});
+
+const ToastAction = forwardRef<ElementRef<"button">, ToastActionProps>(
+	({ asChild, className, onClick, ...props }, ref) => {
+		const ctx = useContext(ToastIdContext);
+
+		const Component = asChild ? Slot : "button";
+
+		return (
+			<Component
+				className={cx("shrink-0", className)}
+				onClick={(event) => {
+					onClick?.(event);
+					if (event.defaultPrevented) {
+						return;
+					}
+					ToastPrimitive.toast.dismiss(ctx);
+				}}
+				ref={ref}
+				{...props}
+			/>
+		);
+	},
+);
+
+type ToastMessageProps = ComponentProps<"p"> & WithAsChild;
+
+const ToastMessage = forwardRef<ElementRef<"p">, ToastMessageProps>(({ asChild, className, ...props }, ref) => {
+	const Component = asChild ? Slot : "p";
+
+	return <Component className={cx("text-strong flex-1 text-sm", className)} ref={ref} {...props} />;
+});
+
+export {
+	//,
+	makeToast,
+	Toast,
+	ToastAction,
+	Toaster,
+	ToastIcon,
+	ToastMessage,
+};
+
+export type {
+	//,
+	Priority,
+};
+
+/**
+ * @private
+ *
+ * Allows any mantle floating prompt (e.g. toasts and notifications) to be interacted with
+ * even when a modaled view (e.g. dialog, sheet, etc) is open and a focus trap is active.
+ *
+ * Without this, interacting with the prompt would close the modaled view.
+ */
+export function preventCloseOnPromptInteraction(
+	event: CustomEvent | PointerEvent | MouseEvent | TouchEvent | FocusEvent,
+) {
+	if (!(event.target instanceof Element)) {
+		return;
+	}
+
+	if (event.target.closest(".mantle-prompt")) {
+		event.preventDefault();
+	}
+}
