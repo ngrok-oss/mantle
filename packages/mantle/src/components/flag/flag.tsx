46087c5e763c2c0d034f5d9d1e1b48191dfe1ccd
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2025-02-18 10:24:17 -0600

    Flag: add border radius and inset transparent black border (#443)
    * Flag: add border radius and inset transparent black border
    
    * typesafety

--- packages/mantle/src/components/flag/flag.tsx
+++ packages/mantle/src/components/flag/flag.tsx
@@ -1,3 +1,4 @@
+import { cva } from "class-variance-authority";
 import type { ComponentProps } from "react";
 import { cx } from "../../utils/cx/cx.js";
 import type { Flags } from "./types.js";
@@ -26,6 +27,26 @@ type Props = Omit<ComponentProps<"div">, "children"> & {
 	loading?: ComponentProps<"img">["loading"];
 };
 
+const borderRadiusVariants = cva("", {
+	variants: {
+		size: {
+			s: "rounded-[0.0625rem]",
+			m: "rounded-[0.09375rem]",
+			l: "rounded-sm",
+		} as const satisfies Record<Size, string>,
+	},
+});
+
+const sizingVariants = cva("", {
+	variants: {
+		size: {
+			s: "w-4 h-3",
+			m: "w-5 h-[0.9375rem]",
+			l: "w-8 h-6",
+		} as const satisfies Record<Size, string>,
+	},
+});
+
 /**
  * A flag component that displays a flag based on the provided country code.
  * Inspired by [react-flagpack](https://flagpack.xyz/docs/development/react).
@@ -38,18 +59,23 @@ function Flag({
 	loading = "lazy",
 	...props
 }: Props) {
+	const borderRadius = borderRadiusVariants({ size });
+	const sizing = sizingVariants({ size });
+
 	return (
 		<div
 			className={cx(
-				"flag",
-				size === "s" && "w-4 h-3",
-				size === "m" && "w-5 h-[0.9375rem]",
-				size === "l" && "w-8 h-6",
+				"flag relative overflow-hidden",
+				borderRadius,
+				sizing,
 				className,
 			)}
 			{...props}
 		>
-			{/* Depend on the build configs to make the assets available at this location */}
+			<div
+				aria-hidden
+				className={cx("absolute inset-0 border border-[#000]/10", borderRadius)}
+			/>
 			<img
 				className="h-full w-full block object-cover"
 				src={`${cdnOrigin}/flags/${size}/${code}.svg`}
