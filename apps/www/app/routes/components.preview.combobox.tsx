504e2d662dcea848e6c5cbf4dfcdb7225bf4812d
Cody A Price <cody@ngrok.com>
2025-02-19 10:12:57 -0600

    real fake doc site page

--- apps/www/app/routes/components.preview.combobox.tsx
+++ apps/www/app/routes/components.preview.combobox.tsx
@@ -92,33 +92,49 @@ export default function Page() {
 						<CodeBlockCode
 							language="tsx"
 							value={fmtCode`
-								import { Button } from "@ngrok/mantle/button";
-								import { HoverCard, HoverCardContent, HoverCardTrigger } from "@ngrok/mantle/hover-card";
-								import { Calendar } from "@phosphor-icons/react/Calendar";
-								import { Shrimp } from "@phosphor-icons/react/Shrimp";
+								import {
+									Combobox,
+									ComboboxContent,
+									ComboboxGroup,
+									ComboboxGroupLabel,
+									ComboboxInput,
+									ComboboxItem,
+									ComboboxItemValue,
+									ComboboxSeparator,
+								} from "@ngrok/mantle/combobox";
+								import { CirclesThreePlus } from "@phosphor-icons/react/CirclesThreePlus";
 
-								<HoverCard>
-									<HoverCardTrigger asChild>
-										<Button type="button" appearance="link">
-											Open Hover Card
-										</Button>
-									</HoverCardTrigger>
-									<HoverCardContent className="w-80">
-										<div className="flex justify-between space-x-4">
-											<div className="flex size-16 shrink-0 items-center justify-center rounded-full bg-pink-300">
-												<Shrimp className="size-12" />
-											</div>
-											<div className="space-y-1">
-												<h4 className="text-sm font-semibold">@ngrok/mantle</h4>
-												<p className="text-sm">The Design System – created and maintained by @ngrok.</p>
-												<div className="flex items-center pt-2">
-													<Calendar className="mr-2 h-4 w-4 opacity-70" />{" "}
-													<span className="text-muted-foreground text-xs">Joined November 2023</span>
-												</div>
-											</div>
-										</div>
-									</HoverCardContent>
-								</HoverCard>
+								<Combobox>
+									<ComboboxInput />
+									<ComboboxContent>
+										<ComboboxGroup>
+											<ComboboxGroupLabel>
+												Choose an ngrok subdomain
+											</ComboboxGroupLabel>
+											<ComboboxItem value="https://" disabled>
+												<ComboboxItemValue />
+											</ComboboxItem>
+											<ComboboxItem value="https://\${random}.ngrok.app">
+												<CirclesThreePlus
+													weight="duotone"
+													className="text-accent-600"
+												/>
+												<ComboboxItemValue />
+											</ComboboxItem>
+											<ComboboxItem value="https://\${random}.ngrok.dev">
+												<ComboboxItemValue />
+											</ComboboxItem>
+											<ComboboxItem value="https://\${random}.ngrok.io">
+												<ComboboxItemValue />
+											</ComboboxItem>
+										</ComboboxGroup>
+										<ComboboxSeparator />
+										<ComboboxItem>
+											Sit dolor enim eiusmod nulla nostrud officia in magna deserunt
+											ut ex veniam cillum.
+										</ComboboxItem>
+									</ComboboxContent>
+								</Combobox>
 							`}
 						/>
 					</CodeBlockBody>
