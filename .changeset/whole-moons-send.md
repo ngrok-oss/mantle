f435325310d4cdefc0939244ca07bc1e180662a6
Cody A Price <cody@ngrok.com>
2025-02-21 13:01:03 -0600

    CodeBlock: fix alignment of absolutely positioned copy button

---
+++ .changeset/whole-moons-send.md
@@ -0,0 +1,5 @@
+---
+"@ngrok/mantle": patch
+---
+
+CodeBlock: fix alignment of absolutely positioned copy button
--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	server: {
-		port: 3000,
+		port: 3333,
 	},
 	plugins: [
 		//,
