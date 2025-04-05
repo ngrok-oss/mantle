269cc54bc529aab6a365a7fc5a0d1d373e6b4020
Cody A Price <cody@ngrok.com>
2025-04-05 13:17:03 -0500

    PopoverContent: prevent click events from bubbling out of the PopoverContent container

--- packages/mantle/src/components/popover/popover.tsx
+++ packages/mantle/src/components/popover/popover.tsx
@@ -60,20 +60,40 @@ PopoverClose.displayName = "PopoverClose";
 const PopoverContent = forwardRef<
 	ComponentRef<typeof PopoverPrimitive.Content>,
 	ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-	<PopoverPrimitive.Portal>
-		<PopoverPrimitive.Content
-			ref={ref}
-			align={align}
-			sideOffset={sideOffset}
-			className={cx(
-				"text-popover-foreground border-popover bg-popover data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95 z-50 w-72 rounded-md border p-4 shadow-md outline-none",
-				className,
-			)}
-			{...props}
-		/>
-	</PopoverPrimitive.Portal>
-));
+>(
+	(
+		{
+			//,
+			align = "center",
+			className,
+			onClick,
+			sideOffset = 4,
+			...props
+		},
+		ref,
+	) => (
+		<PopoverPrimitive.Portal>
+			<PopoverPrimitive.Content
+				align={align}
+				className={cx(
+					"text-popover-foreground border-popover bg-popover data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 data-state-open:animate-in data-state-open:fade-in-0 data-state-open:zoom-in-95 z-50 w-72 rounded-md border p-4 shadow-md outline-none",
+					className,
+				)}
+				onClick={(event) => {
+					/**
+					 * Prevent the click event from propagating up to parent/containing elements
+					 * of the PopoverContent
+					 */
+					event.stopPropagation();
+					onClick?.(event);
+				}}
+				ref={ref}
+				sideOffset={sideOffset}
+				{...props}
+			/>
+		</PopoverPrimitive.Portal>
+	),
+);
 PopoverContent.displayName = "PopoverContent";
 
 export {
