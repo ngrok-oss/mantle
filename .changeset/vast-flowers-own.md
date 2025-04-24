44cc9b9d3eef4f09a87579350e12d12fcf7d78aa
Cody Price <34115417+cody-dot-js@users.noreply.github.com>
2025-04-24 18:43:20 -0500

    DataTable: fix hover styles, add DataTableActionCell (#546)
    * DataTable: fix hover styles, add DataTableActionCell
    
    * fix alignment
    
    * fix content glob
    
    * changeset

---
+++ .changeset/vast-flowers-own.md
@@ -0,0 +1,5 @@
+---
+"@ngrok/mantle": patch
+---
+
+Tailwind Preset: fix content glob, should point to dist folder now
--- packages/mantle/src/components/data-table/index.ts
+++ packages/mantle/src/components/data-table/index.ts
@@ -3,11 +3,12 @@ export * from "@tanstack/react-table";
 export {
 	//,
 	DataTable,
+	DataTableActionCell,
 	DataTableBody,
+	DataTableEmptyRow,
 	DataTableHead,
 	DataTableHeader,
 	DataTableHeaderSortButton,
 	DataTableRow,
 	DataTableRows,
-	EmptyDataTableRow,
 } from "./data-table.js";
