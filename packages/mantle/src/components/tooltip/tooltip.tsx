ef054ccfba1ae4e83959fd44d605517e52f9c0af
Aaron Shekey <aaron@aaronshekey.com>
2025-06-02 12:58:31 -0500

    Force tooltips to be sans-serif
    In a monospace table, these should be sans serif since they're part of the chrome layer.

--- packages/mantle/src/components/tooltip/tooltip.tsx
+++ packages/mantle/src/components/tooltip/tooltip.tsx
@@ -57,7 +57,7 @@ const TooltipContent = forwardRef<
 		ref={ref}
 		sideOffset={sideOffset}
 		className={cx(
-			"bg-tooltip text-tooltip animate-in fade-in-0 zoom-in-95 data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 z-50 max-w-72 overflow-hidden break-words rounded-md px-3 py-1.5 text-sm shadow",
+			"bg-tooltip text-tooltip animate-in fade-in-0 zoom-in-95 data-side-bottom:slide-in-from-top-2 data-side-left:slide-in-from-right-2 data-side-right:slide-in-from-left-2 data-side-top:slide-in-from-bottom-2 data-state-closed:animate-out data-state-closed:fade-out-0 data-state-closed:zoom-out-95 z-50 max-w-72 overflow-hidden break-words rounded-md px-3 py-1.5 text-sm shadow font-sans",
 			className,
 		)}
 		{...props}
