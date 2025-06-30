ed858bc3e45de5812c24c4d7bb0748ebf2f8d969
Aaron Shekey <aaron@aaronshekey.com>
2025-06-30 13:21:52 -0500

    Actually bump Tailwind

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,15 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	optimizeDeps: {
 		exclude: ["@ngrok/mantle"],
 	},
 	plugins: [
-		//,
 		reactRouter(),
 		tsconfigPaths(),
+		tailwindcss() as any,
 	],
 	resolve: {
 		conditions: ["@ngrok/mantle/source"],
