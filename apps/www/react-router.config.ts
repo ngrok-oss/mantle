3c8ac41d32662eae2ec00c3f972be0c45bb5a821
Cody A Price <cody@ngrok.com>
2025-07-17 13:25:17 -0500

    update config

--- apps/www/react-router.config.ts
+++ apps/www/react-router.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 
+const isVercelDeploy = process.env.VERCEL === "1";
+
 export default {
 	ssr: true,
-	presets: [vercelPreset()],
+	presets: [
+		//,
+		isVercelDeploy && vercelPreset(),
+	].filter(Boolean),
 } satisfies Config;
