226671a4f2b5821f72a216c4c5e498e820457648
Cody A Price <cody@ngrok.com>
2025-08-20 21:38:13 -0500

    export useIsHydrated

--- packages/mantle/src/hooks/index.ts
+++ packages/mantle/src/hooks/index.ts
@@ -5,3 +5,4 @@ export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect.js";
 export { useMatchesMediaQuery } from "./use-matches-media-query.js";
 export { usePrefersReducedMotion } from "./use-prefers-reduced-motion.js";
 export { useRandomStableId } from "./use-random-stable-id.js";
+export { useIsHydrated } from "./use-is-hydrated.js";
