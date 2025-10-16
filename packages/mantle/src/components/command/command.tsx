73e4ecbe96d9282131d2d7e84469951be877bbfd
Rand Seay <rand.seay@gmail.com>
2025-10-16 14:08:39 -0700

    Refine the command palette styles.

--- packages/mantle/src/components/command/command.tsx
+++ packages/mantle/src/components/command/command.tsx
@@ -19,7 +19,7 @@ const CommandRoot = forwardRef<
 		ref={ref}
 		data-slot="command"
 		className={cx(
-			"bg-popover text-popover-foreground flex h-full w-full flex-col overflow-hidden rounded-md",
+			"bg-popover flex h-full w-full flex-col overflow-hidden rounded-md",
 			className,
 		)}
 		{...props}
@@ -45,11 +45,15 @@ const CommandDialog = ({
 			<Dialog.Title>{title}</Dialog.Title>
 			<Dialog.Description>{description}</Dialog.Description>
 		</Dialog.Header>
-		<Dialog.Content className={cx("overflow-hidden p-0", className)}>
-			{showCloseButton && <Dialog.CloseIconButton />}
-			<CommandRoot className="[&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+		<Dialog.Content className={cx("overflow-hidden p-0 relative", className)}>
+			<CommandRoot className="[&_[cmdk-group-heading]]:text-muted **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
 				{children}
 			</CommandRoot>
+			{showCloseButton && (
+				<div className="absolute top-1.5 right-1.5">
+					<Dialog.CloseIconButton />
+				</div>
+			)}
 		</Dialog.Content>
 	</Dialog.Root>
 );
@@ -62,13 +66,13 @@ const CommandInput = forwardRef<
 	<div
 		ref={ref}
 		data-slot="command-input-wrapper"
-		className="flex h-9 items-center gap-2 border-b px-3"
+		className="flex h-9 items-center gap-2 border-b border-popover px-3"
 	>
 		<MagnifyingGlassIcon className="size-4 shrink-0 opacity-50" />
 		<CommandPrimitive.Input
 			data-slot="command-input"
 			className={cx(
-				"placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
+				"placeholder:text-muted flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
 				className,
 			)}
 			{...props}
@@ -114,7 +118,7 @@ const CommandGroup = forwardRef<
 		ref={ref}
 		data-slot="command-group"
 		className={cx(
-			"text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium",
+			"[&_[cmdk-group-heading]]:text-muted overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium",
 			className,
 		)}
 		{...props}
@@ -129,7 +133,10 @@ const CommandSeparator = forwardRef<
 	<CommandPrimitive.Separator
 		ref={ref}
 		data-slot="command-separator"
-		className={cx("bg-border -mx-1 h-px", className)}
+		className={cx(
+			"dark-high-contrast:bg-black high-contrast:bg-black bg-gray-500/20 dark:bg-gray-600/20 -mx-1 h-px",
+			className,
+		)}
 		{...props}
 	/>
 ));
@@ -143,7 +150,7 @@ const CommandItem = forwardRef<
 		ref={ref}
 		data-slot="command-item"
 		className={cx(
-			"data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+			"data-[selected=true]:bg-popover-hover [&_svg:not([class*='text-'])]:text-muted relative flex cursor-pointer items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
 			className,
 		)}
 		{...props}
@@ -158,10 +165,7 @@ const CommandShortcut = forwardRef<
 	<span
 		ref={ref}
 		data-slot="command-shortcut"
-		className={cx(
-			"text-muted-foreground ml-auto text-xs tracking-widest",
-			className,
-		)}
+		className={cx("text-muted ml-auto text-xs tracking-widest", className)}
 		{...props}
 	/>
 ));
