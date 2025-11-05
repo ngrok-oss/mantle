2b73efe330f3f01757c253aef922fac9ded88d49
Cody A Price <cody@ngrok.com>
2025-11-04 20:13:47 -0600

    Update Kbd bg color

---
+++ .changeset/wise-melons-enter.md
@@ -0,0 +1,5 @@
+---
+"@ngrok/mantle": patch
+---
+
+Update Kbd bg color
--- packages/mantle/src/components/kbd/kbd.tsx
+++ packages/mantle/src/components/kbd/kbd.tsx
@@ -19,7 +19,7 @@ function Kbd({ children, className, ...props }: ComponentProps<"kbd">) {
 		<kbd
 			className={cx(
 				"[font-kerning:normal] [font-variant-ligatures:common-ligatures_contextual]",
-				"appearance-none tabular-nums inline-grid place-items-center size-5 bg-neutral-200 px-1 rounded text-mono leading-none font-mono",
+				"appearance-none tabular-nums inline-grid place-items-center size-5 bg-neutral-500/15 px-1 rounded text-mono leading-none font-mono",
 				className,
 			)}
 			{...props}
