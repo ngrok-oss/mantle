5b8708e28079168e07bc7fb8cb4211009fa70d23
Aaron Shekey <aaron@aaronshekey.com>
2025-12-09 10:59:13 -0600

    More generic

--- apps/www/app/components/page-header.tsx
+++ apps/www/app/components/page-header.tsx
@@ -1,12 +1,13 @@
-import { cx } from "@ngrok/mantle/cx";
 import type { ComponentProps } from "react";
 import { PreviewBadge, UnreleasedBadge } from "./badges";
 import { HashLinkHeading } from "./hash-link-heading";
 
 type PageHeaderProps = Omit<ComponentProps<"h1">, "id"> & {
 	id: string;
+	title: string;
 	isPreview?: boolean;
 	isUnreleased?: boolean;
+	description?: string;
 };
 
 export const PageHeader = ({
@@ -15,15 +16,20 @@ export const PageHeader = ({
 	isPreview,
 	isUnreleased,
 	id,
+	title,
+	description,
 	...props
 }: PageHeaderProps) => (
-	<div className="flex flex-wrap items-center gap-3">
-		<HashLinkHeading id={id}>
-			<h1 className={cx("", className)} {...props}>
-				{children}
-			</h1>
-		</HashLinkHeading>
-		{isPreview && <PreviewBadge />}
-		{isUnreleased && <UnreleasedBadge />}
+	<div>
+		<div className="flex flex-wrap items-center gap-3">
+			<HashLinkHeading id={id}>
+				<h1 className="mb-0">{title}</h1>
+			</HashLinkHeading>
+			{isPreview && <PreviewBadge />}
+			{isUnreleased && <UnreleasedBadge />}
+		</div>
+		{description && (
+			<p className="mt-4 text-xl font-body text-body">{description}</p>
+		)}
 	</div>
 );
