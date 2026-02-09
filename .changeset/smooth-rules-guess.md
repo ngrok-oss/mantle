e26feaef8d9ae3a7a9e39e9f9ce555ae46d3dafa
Cody A Price <cody@ngrok.com>
2026-02-09 16:59:54 -0600

    Add CssProperties, $cssProperties type and helper for supporting typechecking of react CSSProperties AND custom css properties (--foo-bar)

---
+++ .changeset/smooth-rules-guess.md
@@ -0,0 +1,5 @@
+---
+"@ngrok/mantle": patch
+---
+
+Add CssProperties, $cssProperties type and helper for supporting typechecking of react CSSProperties AND custom css properties (--foo-bar)
--- packages/mantle/src/types/index.ts
+++ packages/mantle/src/types/index.ts
@@ -1,10 +1,8 @@
-export {
-	//,
-	parseBooleanish,
-} from "./booleanish.js";
+export { parseBooleanish } from "./booleanish.js";
 
 export type { WithAsChild } from "./as-child.js";
 export type { WithStyleProps } from "./with-style-props.js";
 export type { VariantProps } from "./variant-props.js";
 export type { DeepNonNullable } from "./deep-non-nullable.js";
 export type { Booleanish } from "./booleanish.js";
+export { $cssProperties, type CssProperties } from "./css-properties.js";
