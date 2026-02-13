75470ebc4808a258f7625d775540d74801b072ae
Cody A Price <cody@ngrok.com>
2026-02-12 18:54:24 -0600

    DescriptionList styling fixes

--- packages/mantle/src/components/description-list/description-list.tsx
+++ packages/mantle/src/components/description-list/description-list.tsx
@@ -32,7 +32,7 @@ const Root = forwardRef<ComponentRef<"dl">, DescriptionListProps>(
 			<Component
 				ref={ref}
 				className={cx(
-					"relative scrollbar overflow-x-auto overscroll-x-none rounded-lg border border-card divide-y divide-card-muted grid grid-cols-[auto_1fr] [&>*:nth-child(odd)]:bg-base",
+					"relative scrollbar overflow-x-auto overscroll-x-none rounded-lg border border-card grid grid-cols-[auto_1fr] gap-x-4 [&>*:nth-child(odd)]:bg-base p-1",
 					className,
 				)}
 				{...rest}
@@ -68,10 +68,7 @@ const Item = forwardRef<ComponentRef<"div">, DescriptionListItemProps>(
 		return (
 			<Component
 				ref={ref}
-				className={cx(
-					"first:rounded-t-lg last:rounded-b-lg col-span-full grid grid-cols-subgrid items-center",
-					className,
-				)}
+				className={cx("rounded-xs col-span-full grid grid-cols-subgrid items-center", className)}
 				{...rest}
 			>
 				{children}
@@ -100,7 +97,7 @@ const Label = forwardRef<ComponentRef<"dt">, DescriptionListLabelProps>(
 		return (
 			<Component
 				ref={ref}
-				className={cx("text-muted text-mono font-sans p-3", className)}
+				className={cx("text-muted text-sm font-sans font-semibold min-w-36 p-3", className)}
 				{...rest}
 			>
 				{children}
@@ -131,7 +128,7 @@ const Value = forwardRef<ComponentRef<"dd">, DescriptionListValueProps>(
 		const Component = asChild ? Slot : "dd";
 
 		return (
-			<Component ref={ref} className={cx("text-body font-mono text-sm p-3", className)} {...rest}>
+			<Component ref={ref} className={cx("text-body font-mono text-mono p-3", className)} {...rest}>
 				{children}
 			</Component>
 		);
