b515cf8e65c9f7c8831cd021da3c3af726d1e691
Cody A Price <cody@ngrok.com>
2026-02-27 05:23:28 -0600

    add useInView hook, reorg code/exports for utils

---
+++ packages/mantle/src/utils/index.ts
@@ -0,0 +1,35 @@
+export { inView } from "./in-view.js";
+export type { InViewOptions, MarginType, ViewChangeHandler } from "./in-view.js";
+
+export { composeRefs } from "./compose-refs/compose-refs.js";
+
+export {
+	//,
+	compareDatesNewestToOldest,
+	compareDatesOldestToNewest,
+} from "./sorting/compare.js";
+
+export {
+	//,
+	$alphanumericSortingDirection,
+	$sortingDirection,
+	$sortingMode,
+	$timeSortingDirection,
+	alphanumericSortingDirections,
+	isAlphanumericSortingDirection,
+	isSortingDirection,
+	isSortingMode,
+	isTimeSortingDirection,
+	sortingDirections,
+	sortingModes,
+	timeSortingByDirection,
+	timeSortingDirections,
+} from "./sorting/direction.js";
+
+export type {
+	//,
+	AlphanumericSortingDirection,
+	SortingDirection,
+	SortingMode,
+	TimeSortingDirection,
+} from "./sorting/direction.js";
